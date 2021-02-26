9ea33ce79032397b743aba50be20a68c5ace768a
kelvinht-373 <kelvin373.ht@gmail.com>
2021-02-26 13:15:04 +0700

    PrintManagerUrovo: No Success Print Out

--- PrintManagerUrovo/app/src/main/java/com/tsm/printmanagerurovo/MainActivity.kt
+++ PrintManagerUrovo/app/src/main/java/com/tsm/printmanagerurovo/MainActivity.kt
@@ -1,11 +1,33 @@
 package com.tsm.printmanagerurovo
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.os.Message
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    companion object {
+        const val PRINT_TEXT = 0
+    }
+    private val mPrintHandler: Handler? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btn_print.setOnClickListener {
+            val content = """
+                    Print test content!
+                    0123456789
+                    abcdefghijklmnopqrstuvwsyz
+                    ABCDEFGHIJKLMNOPQRSTUVWSYZ
+                    """.trimIndent()
+
+            val msg: Message? = mPrintHandler?.obtainMessage(PRINT_TEXT)
+            msg?.obj = content
+            msg?.sendToTarget()
+        }
     }
 }
