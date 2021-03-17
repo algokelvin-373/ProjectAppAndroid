1b993f923483db723731811194205ac6d36e04b4
kelvinht-373 <kelvin373.ht@gmail.com>
2021-03-17 17:00:44 +0700

    WizardPosApp: Create Method DetectCardThread

--- WizardPosDevelopment/app/src/main/java/com/algokelvin/wizardpos/MainActivity.kt
+++ WizardPosDevelopment/app/src/main/java/com/algokelvin/wizardpos/MainActivity.kt
@@ -1,14 +1,11 @@
 package com.algokelvin.wizardpos
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import com.algokelvin.wizardpos.databinding.ActivityMainBinding
 import com.cloudpos.Device
-import com.cloudpos.OperationResult
 import com.cloudpos.POSTerminal
-import com.cloudpos.TimeConstants
-import com.cloudpos.msr.MSRDevice
 
 class MainActivity : AppCompatActivity() {
     private lateinit var mainBinding: ActivityMainBinding
@@ -23,57 +20,10 @@ class MainActivity : AppCompatActivity() {
 
         device = POSTerminal.getInstance(this).getDevice("cloudpos.device.msr")
         if (statusOnDevice == 0) {
-            runOnUiThread {
-                Log.i("WizardPos", statusOnDevice.toString())
-                val threadDetectCard = DetectCardThread()
-                threadDetectCard.start()
-                Log.i("WizardPos", statusOnDevice.toString())
-            }
-        }
-    }
-    private inner class DetectCardThread : Thread() {
-        override fun run() {
-            try {
-                device.open()
-                statusOnDevice = 1
-                Log.i("WizardPos", "run: MSR is running")
-                val operationResult = (device as MSRDevice).waitForSwipe(TimeConstants.FOREVER)
-                if (operationResult.resultCode == OperationResult.SUCCESS) {
-                    val trackData = operationResult.msrTrackData
-
-                    // This is define to data Credit Card
-                    val keyCreditCard0 = String(trackData.getTrackData(0))
-                    val keyCreditCard1 = String(trackData.getTrackData(1))
-
-                    // This is section to Determine Name and Deadline Credit Card
-                    mainBinding.dataCard0.text = ("dataCard 0 : $keyCreditCard0")
-                    if (trackData.getTrackData(0) != null) {
-                        Log.i("WizardPos","dataCard 0 : $keyCreditCard0")
-                    }
-                    else {
-                        Log.i("WizardPos","dataCard 0 : Kosong")
-                    }
-
-                    // This is to get code and CVV Credit Card
-                    mainBinding.dataCard1.text = ("dataCard 1 : $keyCreditCard1")
-                    if (trackData.getTrackData(1) != null) {
-                        Log.i("WizardPos","dataCard 1 : $keyCreditCard1")
-                    }
-                }
-            }
-            catch (e: Exception) {
-                e.printStackTrace()
-                Log.i("WizardPos","dataCard : ${e.message}")
-            }
-            finally {
-                try {
-                    device.close()
-                    Log.i("WizardPos", "run: MSR Mati")
-                }
-                catch (e: Exception) {
-                    e.printStackTrace()
-                }
-            }
+            Log.i("WizardPos", statusOnDevice.toString())
+            val threadDetectCard = DetectCardThread(device, statusOnDevice)
+            threadDetectCard.start()
+            Log.i("WizardPos", statusOnDevice.toString())
         }
     }
 }
