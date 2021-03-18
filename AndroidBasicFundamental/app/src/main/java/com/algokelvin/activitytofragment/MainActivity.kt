80798951ca0ede02fd28b027f4c394442dc81a96
kelvinht-373 <kelvin373.ht@gmail.com>
2021-03-18 12:45:19 +0700

    AndroidBasicFundamental: Program ActivityToFragment - Set HomeFragment

--- AndroidBasicFundamental/app/src/main/java/com/algokelvin/activitytofragment/MainActivity.kt
+++ AndroidBasicFundamental/app/src/main/java/com/algokelvin/activitytofragment/MainActivity.kt
@@ -7,5 +7,16 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val mFragmentManager = supportFragmentManager
+        val mHomeFragment = HomeFragment()
+        val fragment = mFragmentManager.findFragmentByTag(HomeFragment::class.java.simpleName)
+
+        if (fragment !is HomeFragment) {
+            mFragmentManager
+                .beginTransaction()
+                .add(R.id.frame_container, mHomeFragment, HomeFragment::class.java.simpleName)
+                .commit()
+        }
     }
 }
