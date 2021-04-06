94392337d223ec24a7b55054bd3c37b88499d882
kelvinht-373 <kelvin373.ht@gmail.com>
2021-04-06 10:17:49 +0700

    AndroidBasicFundamental: Program ActivityToFragment - Create Program Fragment Basic (No Fix)

---
+++ AndroidBasicFundamental/app/src/main/java/com/algokelvin/activitytofragment/PageOneFragment.kt
@@ -0,0 +1,18 @@
+package com.algokelvin.activitytofragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class PageOneFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_page_one, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
