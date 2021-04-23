4f57e237ae3c5f0b26afff9e3f176c3b725f2001
kelvinht-373 <kelvin373.ht@gmail.com>
2021-04-23 20:56:17 +0700

    AndroidBasicFundamental: Program Animation - Animation Translation Part 01

--- AndroidBasicFundamental/AndroidAnimation/app/src/main/java/com/algokelvin/animationandroid/MainActivity.kt
+++ AndroidBasicFundamental/AndroidAnimation/app/src/main/java/com/algokelvin/animationandroid/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.algokelvin.animationandroid
 
-import androidx.appcompat.app.AppCompatActivity
+import android.animation.ObjectAnimator
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val animation = ObjectAnimator.ofFloat(txt_hello, "translationX", 200f)
+        animation.duration = 2000
+        animation.start()
     }
 }
