299c40ecd19b4c55cee0a9529a8e8d719a987224
kelvin373-ht <kelvinht373@gmail.com>
2021-05-24 10:53:27 +0700

    Android Case Study: Timer Apps - Success Make Program Timer

--- AndroidCaseStudy/TimerApps/app/src/main/java/com/algokelvin/timer/MainActivity.java
+++ AndroidCaseStudy/TimerApps/app/src/main/java/com/algokelvin/timer/MainActivity.java
@@ -3,12 +3,41 @@ package com.algokelvin.timer;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private CountDownTimer timer = null;
+    private TextView tvTimer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tvTimer = findViewById(R.id.tv_timer);
+
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                createTimer(600, 100); // 1 minute
+            }
+        });
+    }
+    private void createTimer(int delay, int maxLoop) {
+        timer = new CountDownTimer((long) delay * maxLoop, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                long minutes = millisUntilFinished / 1000 / 60;
+                long seconds = millisUntilFinished / 1000 % 60;
+                tvTimer.setText(String.format("%02d:%02d", minutes, seconds));
+            }
+
+            @Override
+            public void onFinish() {
+                // TO DO
+            }
+        };
+        timer.start();
     }
 }
