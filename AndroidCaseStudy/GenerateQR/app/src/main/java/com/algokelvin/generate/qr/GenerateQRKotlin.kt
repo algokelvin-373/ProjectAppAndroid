4495d45a9aa05f879418a339ea0a1a9287c9887b
kelvin373-ht <kelvinht373@gmail.com>
2021-05-24 13:09:45 +0700

    Android Case Study: Generate QR Apps - Create Project + Add Kotlin Envirountment

---
+++ AndroidCaseStudy/GenerateQR/app/src/main/java/com/algokelvin/generate/qr/GenerateQRKotlin.kt
@@ -0,0 +1,10 @@
+package com.algokelvin.generate.qr
+
+class GenerateQRKotlin {
+    companion object {
+        @kotlin.jvm.JvmStatic
+        fun set() {
+            // Just Test
+        }
+    }
+}
---
+++ AndroidCaseStudy/GenerateQR/app/src/main/java/com/algokelvin/generate/qr/MainActivity.java
@@ -0,0 +1,16 @@
+package com.algokelvin.generate.qr;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        GenerateQRKotlin.set();
+    }
+}
