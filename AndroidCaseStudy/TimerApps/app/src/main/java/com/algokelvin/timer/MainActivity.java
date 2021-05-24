1962cbd5f635b63ddb753014e2ecb2047a942ae2
kelvin373-ht <kelvinht373@gmail.com>
2021-05-24 11:39:17 +0700

    Android Case Study: Timer Apps - Create Method Timer in Kotlin Language

--- AndroidCaseStudy/TimerApps/app/src/main/java/com/algokelvin/timer/MainActivity.java
+++ AndroidCaseStudy/TimerApps/app/src/main/java/com/algokelvin/timer/MainActivity.java
@@ -6,7 +6,7 @@ import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-    private TextView tvTimer, statusTimeOut;
+    private TextView tvTimer, tvTimerJava, statusTimeOut, statusTimeOutJava;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,10 +14,18 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         tvTimer = findViewById(R.id.tv_timer);
+        tvTimerJava = findViewById(R.id.tv_timer_java);
         statusTimeOut = findViewById(R.id.status_time_out);
+        statusTimeOutJava = findViewById(R.id.status_time_out_java);
 
+        // Call Method using Kotlin
         runOnUiThread(() -> {
-            TimerJava.createTimer(tvTimer, statusTimeOut, 50, 100); // 5 seconds
+            TimerKotlin.createTimer(tvTimer, statusTimeOut, 50, 100); // 5 seconds
+        });
+
+        // Call Method using Java
+        runOnUiThread(() -> {
+            TimerJava.createTimer(tvTimerJava, statusTimeOutJava, 100, 100); // 10 seconds
         });
     }
 }
--- AndroidCaseStudy/TimerApps/app/src/main/java/com/algokelvin/timer/TimerJava.java
+++ AndroidCaseStudy/TimerApps/app/src/main/java/com/algokelvin/timer/TimerJava.java
@@ -1,7 +1,6 @@
 package com.algokelvin.timer;
 
 import android.os.CountDownTimer;
-import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
