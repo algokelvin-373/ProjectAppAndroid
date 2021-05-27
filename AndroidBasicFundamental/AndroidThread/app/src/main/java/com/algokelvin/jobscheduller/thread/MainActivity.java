d98beb0d4473e0908efe4427e0b02ba712a95da3
kelvin373-ht <kelvinht373@gmail.com>
2021-05-27 08:01:20 +0700

    Android Basic Fundamental: Thread basic

--- AndroidBasicFundamental/AndroidThread/app/src/main/java/com/algokelvin/jobscheduller/thread/MainActivity.java
+++ AndroidBasicFundamental/AndroidThread/app/src/main/java/com/algokelvin/jobscheduller/thread/MainActivity.java
@@ -3,12 +3,25 @@ package com.algokelvin.jobscheduller.thread;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private String txt = "empty data";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView txtHello = findViewById(R.id.txt_hello);
+        txtHello.setText(txt);
+
+        new Thread(() -> {
+            // a potentially time consuming task
+            Log.i("thread-run", "Thread run");
+            txt = "Thread Run Post";
+            txtHello.setText(txt);
+        }).start();
     }
 }
