b77428ff5cd506da855dd3c1ba0a0d24c74b471a
kelvin373-ht <kelvinht373@gmail.com>
2021-07-06 15:14:55 +0700

    Android Basic Fundamental: SubFolderLayoutApp - Subfolder Layout

---
+++ AndroidBasicFundamental/SubFolderLayoutApp/app/src/main/java/com/algokelvin/subfolderlayout/movie/MoviePageActivity.kt
@@ -0,0 +1,12 @@
+package com.algokelvin.subfolderlayout.movie
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.algokelvin.subfolderlayout.R
+
+class MoviePageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_movie_page)
+    }
+}
