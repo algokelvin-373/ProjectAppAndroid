b48468a5798b6b9fce5204f7a14a78f7fd6872aa
kelvin373-ht <kelvinht373@gmail.com>
2021-07-12 06:10:55 +0700

    Android Basic Fundamental: Gson App - Success Implement Gson

--- AndroidBasicFundamental/GsonApp/app/src/main/java/com/algokelvin/gson/MainActivity.java
+++ AndroidBasicFundamental/GsonApp/app/src/main/java/com/algokelvin/gson/MainActivity.java
@@ -1,8 +1,12 @@
 package com.algokelvin.gson;
 
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.google.gson.Gson;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnParseJson = findViewById(R.id.btnParseJson);
+        TextView strToJson = findViewById(R.id.strToJson);
+        TextView strFromJson = findViewById(R.id.strFromJson);
+
+        Gson gson = new Gson();
+
+        btnParseJson.setOnClickListener(v -> {
+            People People1 = new People("Zia","30","info@theappsfirm.com");
+            String json = gson.toJson(People1);
+            strToJson.setText("Json String: \n"+json);
+
+            String jsonString = "{\"mAge\":\"30\",\"mEmail\":\"info@theappsfirm.com\",\"mFirstName\":\"Zia\"}";
+            People People2 = gson.fromJson(jsonString, People.class);
+            strFromJson.setText(" People: "+People2.mFirstName+" Age "+People2.mAge+" Email "+People2.mEmail);
+        });
+
     }
 }
