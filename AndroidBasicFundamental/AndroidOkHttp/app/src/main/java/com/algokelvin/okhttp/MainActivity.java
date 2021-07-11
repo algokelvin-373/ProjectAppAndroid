2019805092ed47d4dc559002ee5f18ee76be78c8
kelvin373-ht <kelvinht373@gmail.com>
2021-07-11 07:31:34 +0700

    Android Basic Fundamental: OkHttp App - Method 'synchronousGet' and 'asynchronousGet'

--- AndroidBasicFundamental/AndroidOkHttp/app/src/main/java/com/algokelvin/okhttp/MainActivity.java
+++ AndroidBasicFundamental/AndroidOkHttp/app/src/main/java/com/algokelvin/okhttp/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
@@ -12,7 +13,17 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
 import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private Button buttonUpload, buttonDownload, asynchronousGet, synchronousGet, asynchronousPOST;
@@ -58,11 +69,72 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View v) {
-
+        switch (v.getId()) {
+            case R.id.synchronousGet:
+                SynchronousGetRequest okHttpHandler = new SynchronousGetRequest();
+                okHttpHandler.execute(url);
+                break;
+            case R.id.asynchronousGet:
+                asynchronousGet();
+                break;
+        }
     }
 
     @Override
     public void onPointerCaptureChanged(boolean hasCapture) {
 
     }
+
+    private void asynchronousGet() {
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                call.cancel();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                final String myResponse = response.body().string();
+                MainActivity.this.runOnUiThread(() -> {
+                    try {
+                        JSONObject json = new JSONObject(myResponse);
+                        txtString.setText("First Name: "+json.getJSONObject("data").getString("first_name") + "\nLast Name: " + json.getJSONObject("data").getString("last_name"));
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                });
+            }
+        });
+    }
+
+    public class SynchronousGetRequest extends AsyncTask<String, Void, String> {
+        OkHttpClient client = new OkHttpClient();
+
+        @Override
+        protected String doInBackground(String... params) {
+            Request.Builder builder = new Request.Builder();
+            builder.url(params[0]);
+            Request request = builder.build();
+
+            try {
+                Response response = client.newCall(request).execute();
+                return response.body().string();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            txtString.setText(s);
+        }
+    }
+
 }
