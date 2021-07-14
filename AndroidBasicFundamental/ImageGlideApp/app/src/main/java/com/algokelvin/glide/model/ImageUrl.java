739d3812dcf8846fd1423a90450768f466f0c0ab
kelvin373-ht <kelvinht373@gmail.com>
2021-07-14 08:29:33 +0700

    Android Basic Fundamental: Retrofit App - Create and Success Implement Glide

---
+++ AndroidBasicFundamental/ImageGlideApp/app/src/main/java/com/algokelvin/glide/model/ImageUrl.java
@@ -0,0 +1,13 @@
+package com.algokelvin.glide.model;
+
+public class ImageUrl {
+    String imageUrl;
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+}
