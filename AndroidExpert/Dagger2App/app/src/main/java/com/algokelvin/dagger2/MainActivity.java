4ded97ce9db987c2df8d5c1b2e2b3fa77d992fcb
kelvin373-ht <kelvinht373@gmail.com>
2021-07-17 14:43:11 +0700

    Android Expert: Dagger2 App - Success Implement Dagger 2 with Component Dagger

--- AndroidExpert/Dagger2App/app/src/main/java/com/algokelvin/dagger2/MainActivity.java
+++ AndroidExpert/Dagger2App/app/src/main/java/com/algokelvin/dagger2/MainActivity.java
@@ -1,14 +1,22 @@
 package com.algokelvin.dagger2;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Context;
 import android.os.Bundle;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import javax.inject.Inject;
+
+import dagger.android.support.DaggerAppCompatActivity;
+
+public class MainActivity extends DaggerAppCompatActivity {
+    @Inject
+    Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toast.makeText(context, "You Success Implement Dagger2", Toast.LENGTH_LONG).show();
     }
 }
