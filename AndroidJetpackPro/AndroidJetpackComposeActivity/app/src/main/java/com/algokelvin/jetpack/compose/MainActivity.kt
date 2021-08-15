10ab331c50d1ec879e6d8570868d47bee9c91d4c
kelvin373-ht <kelvinht373@gmail.com>
2021-08-15 22:41:24 +0700

    Android Jetpack Compose: Android Jetpack Compose Activity - Success create + show Hello World

--- AndroidJetpackPro/AndroidJetpackComposeActivity/app/src/main/java/com/algokelvin/jetpack/compose/MainActivity.kt
+++ AndroidJetpackPro/AndroidJetpackComposeActivity/app/src/main/java/com/algokelvin/jetpack/compose/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.algokelvin.jetpack.compose
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        setContent {
+            pageActivity()
+        }
+
     }
+}
+
+@Preview
+@Composable
+fun pageActivity() {
+    Text(text = "Hello, Algokelvin. I'm play Jetpack Compose")
 }
