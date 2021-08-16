c0c25aaeba77d04e3ce97e66b0405af9bc9c9f68
kelvin373-ht <kelvinht373@gmail.com>
2021-08-16 21:25:55 +0700

    Android Jetpack Compose: Jetpack Compose One - Create Project

---
+++ AndroidJetpackPro/JetpackComposeOne/app/src/main/java/com/algokelvin/jetpack/componse/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.algokelvin.jetpack.componse
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
