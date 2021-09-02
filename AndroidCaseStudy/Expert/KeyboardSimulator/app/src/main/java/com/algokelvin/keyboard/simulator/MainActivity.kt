9c39c27bd4676b1c54620be38f4a0320385a8d8d
kelvin373-ht <kelvinht373@gmail.com>
2021-09-03 01:33:32 +0700

    Android Case Study Expert: Keyboard Simulator - Create method 'setActionTextKeyboardSimulator'

--- AndroidCaseStudy/Expert/KeyboardSimulator/app/src/main/java/com/algokelvin/keyboard/simulator/MainActivity.kt
+++ AndroidCaseStudy/Expert/KeyboardSimulator/app/src/main/java/com/algokelvin/keyboard/simulator/MainActivity.kt
@@ -19,24 +19,36 @@ class MainActivity : KeyboardController() {
 
         cl_first_name.setOnClickListener {
             txtTwo = txtInputData.text.toString()
-            cl_second_name.setBackgroundResource(R.drawable.bg_menu_white)
-            cl_first_name.setBackgroundResource(R.drawable.bg_menu_grey)
-            cross_second_name.visibility = View.GONE
-            cross_first_name.visibility = View.VISIBLE
-            setControllerInputData(R.id.first_name)
-            setImgCrossInput(R.id.cross_first_name)
+            setActionTextKeyboardSimulator(
+                R.drawable.bg_menu_white,
+                R.drawable.bg_menu_grey,
+                View.GONE, View.VISIBLE,
+                R.id.first_name,
+                R.id.cross_first_name
+            )
             setTextBefore(txtOne)
         }
         cl_second_name.setOnClickListener {
             txtOne = txtInputData.text.toString()
-            cl_second_name.setBackgroundResource(R.drawable.bg_menu_grey)
-            cl_first_name.setBackgroundResource(R.drawable.bg_menu_white)
-            cross_second_name.visibility = View.VISIBLE
-            cross_first_name.visibility = View.GONE
-            setControllerInputData(R.id.second_name)
-            setImgCrossInput(R.id.cross_second_name)
+            setActionTextKeyboardSimulator(
+                R.drawable.bg_menu_grey,
+                R.drawable.bg_menu_white,
+                View.VISIBLE, View.GONE,
+                R.id.second_name,
+                R.id.cross_second_name
+            )
             setTextBefore(txtTwo)
         }
 
     }
+
+    private fun setActionTextKeyboardSimulator(bg1: Int, bg2: Int, v1: Int, v2: Int, txt: Int, cross: Int) {
+        cl_second_name.setBackgroundResource(bg1)
+        cl_first_name.setBackgroundResource(bg2)
+        cross_second_name.visibility = v1
+        cross_first_name.visibility = v2
+        setControllerInputData(txt)
+        setImgCrossInput(cross)
+    }
+
 }
