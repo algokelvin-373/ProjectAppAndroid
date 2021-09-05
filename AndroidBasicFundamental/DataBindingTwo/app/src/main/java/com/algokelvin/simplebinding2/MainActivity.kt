17c1d97252a4e1d2e45f53468f56185acbbe5094
kelvin373-ht <kelvinht373@gmail.com>
2021-09-06 02:51:15 +0700

    Android Fundamental: DataBindingTwo - Create Utils for Set Visibility Progress Bar ( Best Practice )

--- AndroidBasicFundamental/DataBindingTwo/app/src/main/java/com/algokelvin/simplebinding2/MainActivity.kt
+++ AndroidBasicFundamental/DataBindingTwo/app/src/main/java/com/algokelvin/simplebinding2/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.algokelvin.simplebinding2
 
 import android.os.Bundle
-import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import com.algokelvin.simplebinding2.Utils.VIEW_GONE
+import com.algokelvin.simplebinding2.Utils.VIEW_VISIBLE
+import com.algokelvin.simplebinding2.Utils.setVisibleProgressbar
 import com.algokelvin.simplebinding2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -20,12 +22,10 @@ class MainActivity : AppCompatActivity() {
 
     private fun startOrStopProgressBar() {
         binding.apply {
-            if (progressBar.visibility == View.GONE) {
-                progressBar.visibility = View.VISIBLE
-                controlButton.text = ("Stop")
+            if (progressBar.visibility == VIEW_GONE) {
+                setVisibleProgressbar(VIEW_VISIBLE, "Stop")
             } else {
-                progressBar.visibility = View.GONE
-                controlButton.text = ("Start")
+                setVisibleProgressbar(VIEW_GONE, "Start")
             }
         }
     }
