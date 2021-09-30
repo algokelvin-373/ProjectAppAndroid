bc973e24cb778b72813c6fe2b9e22c5c24db340e
kelvin373-ht <kelvinht373@gmail.com>
2021-09-30 23:12:47 +0700

    Android Case Study Expert: RegisterApps - UI Data Page 1

--- AndroidCaseStudy/Expert/RegisterApps/app/src/main/java/com/algokelvin/register/data/fragment/PageOneFragment.java
+++ AndroidCaseStudy/Expert/RegisterApps/app/src/main/java/com/algokelvin/register/data/fragment/PageOneFragment.java
@@ -6,6 +6,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -34,6 +35,18 @@ public class PageOneFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull @NotNull View view, @Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        View vInput1 = view.findViewById(R.id.layout_data_1);
+        TextView txt1 = vInput1.findViewById(R.id.title_input_data);
+        txt1.setText("Nama Lengkap");
+
+        View vInput2 = view.findViewById(R.id.layout_data_2);
+        TextView txt2 = vInput2.findViewById(R.id.title_input_data);
+        txt2.setText("Nama Panggilan");
+
+        View vInput3 = view.findViewById(R.id.layout_data_3);
+        TextView txt3 = vInput3.findViewById(R.id.title_input_data);
+        txt3.setText("Tempat Lahir");
 
         Button btnNext = view.findViewById(R.id.btn_next);
         btnNext.setOnClickListener(v -> {
