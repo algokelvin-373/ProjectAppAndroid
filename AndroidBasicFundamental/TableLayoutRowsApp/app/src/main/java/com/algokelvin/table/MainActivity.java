509961920020b0c58ec3a8419732e67d249e2724
kelvin373-ht <kelvinht373@gmail.com>
2021-09-30 13:37:51 +0700

    Android Case Study Fundamental: TableRowColumns - Make Case 2 TableRowColumns

--- AndroidBasicFundamental/TableLayoutRowsApp/app/src/main/java/com/algokelvin/table/MainActivity.java
+++ AndroidBasicFundamental/TableLayoutRowsApp/app/src/main/java/com/algokelvin/table/MainActivity.java
@@ -1,6 +1,7 @@
 package com.algokelvin.table;
 
 import android.os.Bundle;
+import android.view.Gravity;
 import android.widget.TableLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -18,7 +19,8 @@ public class MainActivity extends AppCompatActivity {
         data[1][0] = "Name : "; data[1][1] = "Kelvin Herwanda Tandrio";
         data[2][0] = "Instagram : "; data[2][1] = "kelvin_373";
 
-        TableLayout tl = (TableLayout) findViewById(R.id.tb_details);
+        // TableLayout Version 1
+        TableLayout tl = findViewById(R.id.tb_details);
 
         int row = data.length;
         int column = data[0].length;
@@ -26,6 +28,21 @@ public class MainActivity extends AppCompatActivity {
         RowColumns rowColumns = new RowColumns(this, row, column);
         rowColumns.setTableLayout(tl);
         rowColumns.setTableData(data);
+
+        // TableLayout Version 2
+        TableLayout tl2 = findViewById(R.id.tb_details_2);
+
+        int row2 = data.length;
+        int column2 = data[0].length;
+
+        RowColumns rowColumns2 = new RowColumns(this, row2, column2, 8);
+        rowColumns2.setBgTextView(R.color.green, R.color.green);
+        rowColumns2.setGravityTextView(0, Gravity.END);
+        rowColumns2.setTextColors(R.color.red, R.color.blue);
+        rowColumns2.setSizesTextView(8, 16);
+        rowColumns2.setFontTextView(0, R.font.montserrat_regular);
+        rowColumns2.setTableLayout(tl2);
+        rowColumns2.setTableData2(data);
 
     }
 }
