3a9d343f692a35681fe31f76d88fe8e60f896202
kelvin373-ht <kelvinht373@gmail.com>
2021-10-05 19:01:25 +0700

    Android Fundamental: Show Dialog - Success Show Dialog Bottom

--- AndroidBasicFundamental/ShowDialog/app/src/main/java/com/algokelvin/showdialog/MainActivity.java
+++ AndroidBasicFundamental/ShowDialog/app/src/main/java/com/algokelvin/showdialog/MainActivity.java
@@ -3,6 +3,9 @@ package com.algokelvin.showdialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.google.android.material.bottomsheet.BottomSheetDialog;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView btnDialogBottom = findViewById(R.id.btn_dialog_bottom);
+
+        btnDialogBottom.setOnClickListener(v -> setDialogBottom());
     }
+
+    private void setDialogBottom() {
+        BottomSheetDialog bottomSheetDialog = new BottomSheetDialog(this);
+        bottomSheetDialog.setContentView(R.layout.dialog_bottom_layout);
+        bottomSheetDialog.show();
+    }
+
 }
