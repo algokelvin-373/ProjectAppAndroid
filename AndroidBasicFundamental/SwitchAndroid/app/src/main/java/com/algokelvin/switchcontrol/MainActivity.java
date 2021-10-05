f0479cade26c7c3c8598a8c4cbc51826b3e09b81
kelvin373-ht <kelvinht373@gmail.com>
2021-10-05 20:58:51 +0700

    Android Fundamental: Switch Android - Success Action Switch Android

--- AndroidBasicFundamental/SwitchAndroid/app/src/main/java/com/algokelvin/switchcontrol/MainActivity.java
+++ AndroidBasicFundamental/SwitchAndroid/app/src/main/java/com/algokelvin/switchcontrol/MainActivity.java
@@ -2,7 +2,10 @@ package com.algokelvin.switchcontrol;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.widget.Switch;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        @SuppressLint("UseSwitchCompatOrMaterialCode")
+        Switch switchAndroid = findViewById(R.id.switch_android);
+        TextView txtCondition = findViewById(R.id.txt_condition);
+
+        switchAndroid.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            if (isChecked) {
+                txtCondition.setText("Switch is ON");
+                txtCondition.setTextColor(getResources().getColor(R.color.green));
+            } else {
+                txtCondition.setText("Switch is OFF");
+                txtCondition.setTextColor(getResources().getColor(R.color.red));
+            }
+        });
     }
 }
