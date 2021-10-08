0b7cb95281dc1eee23ceb7b68399b1283f000532
kelvin373-ht <kelvinht373@gmail.com>
2021-10-08 18:38:59 +0700

    Android Fundamental - SpinnerList(2): Set Sample Data List

--- AndroidBasicFundamental/SpinnerList/app/src/main/java/com/algokelvin/spinnerlist/MainActivity.java
+++ AndroidBasicFundamental/SpinnerList/app/src/main/java/com/algokelvin/spinnerlist/MainActivity.java
@@ -5,21 +5,17 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.algokelvin.spinnerlist.utils.controller.SpinnerFunction;
-
-import java.util.ArrayList;
+import com.algokelvin.spinnerlist.utils.data.DataSample;
 
 public class MainActivity extends AppCompatActivity {
-    private final ArrayList<SampleData> data = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        data.add(new SampleData(1, R.drawable.ic_rectangle, "Rectangle"));
-        data.add(new SampleData(2, R.drawable.ic_ellipse, "Circle"));
-        data.add(new SampleData(3, R.drawable.ic_polygon, "Triangle"));
-        SpinnerFunction spinnerFunction = new SpinnerFunction(this, data, findViewById(R.id.spinner_list));
+        DataSample dataSample = new DataSample();
+        SpinnerFunction spinnerFunction = new SpinnerFunction(this, dataSample.getData(), findViewById(R.id.spinner_list));
         spinnerFunction.setSpinner();
 
     }
