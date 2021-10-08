3b6215fcef7475d96eabe88a92e8d9dffc59017e
kelvin373-ht <kelvinht373@gmail.com>
2021-10-08 22:24:36 +0700

    Android Case Study Expert: RegisterApps(3) - Simulation Show 2 Includes Input Data Programmatically

--- AndroidCaseStudy/Expert/RegisterApps/app/src/main/java/com/algokelvin/register/data/fragment/PageOneFragment.java
+++ AndroidCaseStudy/Expert/RegisterApps/app/src/main/java/com/algokelvin/register/data/fragment/PageOneFragment.java
@@ -1,13 +1,19 @@
 package com.algokelvin.register.data.fragment;
 
+import android.content.Context;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import com.algokelvin.register.data.R;
 import com.algokelvin.register.data.utils.RegisterController;
@@ -27,7 +33,26 @@ public class PageOneFragment extends RegisterController {
     public void onViewCreated(@NonNull @NotNull View view, @Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        int[] idLayout = {R.id.layout_data_1, R.id.layout_data_2, R.id.layout_data_3};
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT
+        );
+        params.setMargins(0, setDp(10), 0, 0);
+
+        LinearLayout clRegister1 = view.findViewById(R.id.cl_register_1);
+
+        View v = View.inflate(getActivity(), R.layout.include_item_data_register, null);
+        TextView txt1 = v.findViewById(R.id.title_input_data);
+        txt1.setText("Full Name");
+        clRegister1.addView(v);
+
+        View v2 = View.inflate(getActivity(), R.layout.include_item_data_register, null);
+        v2.setLayoutParams(params);
+        TextView txt2 = v2.findViewById(R.id.title_input_data);
+        txt2.setText("Nick Name");
+        clRegister1.addView(v2);
+
+        /*int[] idLayout = {R.id.layout_data_1, R.id.layout_data_2, R.id.layout_data_3};
         String[] text = {"Nama Lengkap", "Nama Panggilan", "Tempat Lahir"};
         String[] edtHint = {getFullName(), getNickName(), getPlaceBirth()};
         setSizes(idLayout.length);
@@ -39,8 +64,13 @@ public class PageOneFragment extends RegisterController {
             setNickName(getEditTexts(1).getText().toString());
             setPlaceBirth(getEditTexts(2).getText().toString());
             getOnViewPager().onSetPage(1);
-        });
+        });*/
 
+    }
+
+    private int setDp(int x) {
+        Resources r = getContext().getResources();
+        return (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, x, r.getDisplayMetrics());
     }
 
 }
