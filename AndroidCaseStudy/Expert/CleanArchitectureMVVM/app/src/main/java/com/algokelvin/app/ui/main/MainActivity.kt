7d19edcc027cc1f4a2b6f6864c874947d4a94487
kelvin373-ht <kelvinht373@gmail.com>
2021-10-18 22:26:58 +0700

    Android Case Study Expert: MVVM Clean Architecture - On ViewBinding and set 'binding' in ActivityMainBinding

--- AndroidCaseStudy/Expert/CleanArchitectureMVVM/app/src/main/java/com/algokelvin/app/ui/main/MainActivity.kt
+++ AndroidCaseStudy/Expert/CleanArchitectureMVVM/app/src/main/java/com/algokelvin/app/ui/main/MainActivity.kt
@@ -3,8 +3,11 @@ package com.algokelvin.app.ui.main
 import android.os.Bundle
 import com.algokelvin.app.R
 import com.algokelvin.app.base.BaseActivity
+import com.algokelvin.app.databinding.ActivityMainBinding
 
 class MainActivity : BaseActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
