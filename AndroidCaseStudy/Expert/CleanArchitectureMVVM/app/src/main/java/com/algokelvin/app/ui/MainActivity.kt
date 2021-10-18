e57253ccc69361fb26fad58433bc03dcd71cc625
kelvin373-ht <kelvinht373@gmail.com>
2021-10-18 21:52:16 +0700

    Android Case Study Expert: MVVM Clean Architecture - Create 'BaseActivity' and extends to 'MainActivity'

--- AndroidCaseStudy/Expert/CleanArchitectureMVVM/app/src/main/java/com/algokelvin/app/ui/MainActivity.kt
+++ AndroidCaseStudy/Expert/CleanArchitectureMVVM/app/src/main/java/com/algokelvin/app/ui/MainActivity.kt
@@ -1,9 +1,10 @@
-package com.algokelvin.app
+package com.algokelvin.app.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.algokelvin.app.R
+import com.algokelvin.app.base.BaseActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
