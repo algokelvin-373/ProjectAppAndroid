ef376bdc7bcb7b41917825bfbeff50228cff39ab
kelvin373-ht <kelvinht373@gmail.com>
2021-10-25 17:38:07 +0700

    Android Case Study Expert: Transaction Apps - Implement lib 'com.github.algokelvin-373:simplicity-registration:1.1.1'

--- AndroidCaseStudy/Expert/TransactionProgram/app/src/main/java/com/algokelvin/transaction/MainActivity.java
+++ AndroidCaseStudy/Expert/TransactionProgram/app/src/main/java/com/algokelvin/transaction/MainActivity.java
@@ -2,7 +2,9 @@ package com.algokelvin.transaction;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnReady = findViewById(R.id.btn_ready);
+        btnReady.setOnClickListener(v -> {
+            Intent intentReady = new Intent(this, TransactionActivity.class);
+            startActivity(intentReady);
+        });
+
     }
 }
