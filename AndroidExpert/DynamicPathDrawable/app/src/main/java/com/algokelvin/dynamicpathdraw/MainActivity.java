62cbb2e140d05bb730ff395d6c3830b617b6355c
algokelvin-373 <kelvinht373@gmail.com>
2021-11-26 13:21:01 +0700

    [Android Expert] [Dynamic Color Path] Success Set Color with 'attr' and 'style'

--- AndroidExpert/DynamicPathDrawable/app/src/main/java/com/algokelvin/dynamicpathdraw/MainActivity.java
+++ AndroidExpert/DynamicPathDrawable/app/src/main/java/com/algokelvin/dynamicpathdraw/MainActivity.java
@@ -1,8 +1,12 @@
 package com.algokelvin.dynamicpathdraw;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.view.ContextThemeWrapper;
+import androidx.vectordrawable.graphics.drawable.VectorDrawableCompat;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView imgPath1 = findViewById(R.id.img_path_1);
+
+        ContextThemeWrapper wrapper = new ContextThemeWrapper(this, R.style.DefaultScene1);
+        Drawable drawable = VectorDrawableCompat.create(getResources(), R.drawable.ic_rounded, wrapper.getTheme());
+        imgPath1.setImageDrawable(drawable);
     }
 }
