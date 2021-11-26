b8d2ab8337f183e0633a3d9689de722044acc8b7
algokelvin-373 <kelvinht373@gmail.com>
2021-11-26 14:28:47 +0700

    [Android Expert] [Dynamic Color Path] 3 Path Image Dynamic

--- AndroidExpert/DynamicPathDrawable/app/src/main/java/com/algokelvin/dynamicpathdraw/MainActivity.java
+++ AndroidExpert/DynamicPathDrawable/app/src/main/java/com/algokelvin/dynamicpathdraw/MainActivity.java
@@ -9,6 +9,8 @@ import android.os.Bundle;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+    private ContextThemeWrapper wrapper;
+    private Drawable drawable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,9 +18,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         ImageView imgPath1 = findViewById(R.id.img_path_1);
+        ImageView imgPath2 = findViewById(R.id.img_path_2);
+        ImageView imgPath3 = findViewById(R.id.img_path_3);
 
-        ContextThemeWrapper wrapper = new ContextThemeWrapper(this, R.style.DefaultScene1);
-        Drawable drawable = VectorDrawableCompat.create(getResources(), R.drawable.ic_rounded, wrapper.getTheme());
+        wrapper = new ContextThemeWrapper(this, R.style.DefaultScene1);
+        drawable = VectorDrawableCompat.create(getResources(), R.drawable.ic_rounded, wrapper.getTheme());
         imgPath1.setImageDrawable(drawable);
+
+        wrapper = new ContextThemeWrapper(this, R.style.DefaultScene2);
+        drawable = VectorDrawableCompat.create(getResources(), R.drawable.ic_rounded, wrapper.getTheme());
+        imgPath2.setImageDrawable(drawable);
+
+        wrapper = new ContextThemeWrapper(this, R.style.DefaultScene3);
+        drawable = VectorDrawableCompat.create(getResources(), R.drawable.ic_rounded, wrapper.getTheme());
+        imgPath3.setImageDrawable(drawable);
     }
 }
