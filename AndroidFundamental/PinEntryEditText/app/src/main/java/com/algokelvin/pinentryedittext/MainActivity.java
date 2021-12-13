9a9d5e626983b55cdd65de00b94c62366e8f3c94
algokelvin-373 <kelvinht373@gmail.com>
2021-12-13 12:35:09 +0700

    [Android Fundamental] [PinEntryEditTest] Success(1)

--- AndroidFundamental/PinEntryEditText/app/src/main/java/com/algokelvin/pinentryedittext/MainActivity.java
+++ AndroidFundamental/PinEntryEditText/app/src/main/java/com/algokelvin/pinentryedittext/MainActivity.java
@@ -3,6 +3,10 @@ package com.algokelvin.pinentryedittext;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.lamonjush.pinentryedittext.PinEntryEditText;
+import com.lamonjush.pinentryedittext.PinEntryListener;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        PinEntryEditText pinEntryEditText = findViewById(R.id.pinEntryEditText);
+        pinEntryEditText.setPinEntryListener(
+                new PinEntryListener() {
+                    @Override
+                    public void onPinEntered(String pin) {
+                        Toast.makeText(MainActivity.this,pin,Toast.LENGTH_SHORT).show();
+                    }
+                }
+        );
     }
 }
