81dfb495004d9e23e43954bce42c82f289e6ac49
algokelvin-373 <kelvinht373@gmail.com>
2022-01-21 13:34:18 +0700

    [Android Animator] Create AnimatorController and Implement

---
+++ AndroidFundamental/AndroidAnimation/app/src/main/java/com/algokelvin/animationandroid/translation/AnimatorController.java
@@ -0,0 +1,17 @@
+package com.algokelvin.animationandroid.translation;
+
+import android.animation.ObjectAnimator;
+import android.widget.TextView;
+
+public class AnimatorController {
+    public static final String TRANSLATE_X  = "translationX";
+
+    private ObjectAnimator animator;
+
+    public void animatorTextView(TextView idText, String animation, float coordinate, int duration) {
+        this.animator = ObjectAnimator.ofFloat(idText, animation, coordinate);
+        animator.setDuration(duration);
+        animator.start();
+    }
+
+}
