4a894fe385b7a02cb3a610e7f9c3f8ba7ec95116
algokelvin-373 <kelvinht373@gmail.com>
2022-02-08 14:01:28 +0700

    [Android Beginner] [Checking Connection] Delete App Code

--- AndroidCaseStudy/Fundamental/CheckingConnection/app/src/main/java/com/algokelvin/checkconnectapps/MainActivity.kt
+++ AndroidCaseStudy/Fundamental/CheckingConnection/app/src/main/java/com/algokelvin/checkconnectapps/MainActivity.kt
@@ -4,7 +4,6 @@ import android.content.BroadcastReceiver
 import android.content.IntentFilter
 import android.net.ConnectivityManager
 import android.os.Bundle
-import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.algokelvin.checkconnectapps.databinding.ActivityMainBinding
--- AndroidCaseStudy/Fundamental/CheckingConnection/app/src/main/java/com/algokelvin/checkconnectapps/MyReceiver.kt
+++ AndroidCaseStudy/Fundamental/CheckingConnection/app/src/main/java/com/algokelvin/checkconnectapps/MyReceiver.kt
@@ -4,7 +4,6 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.net.ConnectivityManager
-import android.widget.Toast
 
 class MyReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context, intent: Intent) {
