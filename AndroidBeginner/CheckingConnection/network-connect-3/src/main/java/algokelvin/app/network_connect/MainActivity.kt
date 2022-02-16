1db5e77382552c988a6f40885d8ccb717df52fcd
algokelvin-373 <kelvinht373@gmail.com>
2022-02-16 14:34:03 +0700

    [Android Beginner] [Checking Connection] Success Get Status Connection with Layout Warning

--- AndroidBeginner/CheckingConnection/network-connect-3/src/main/java/algokelvin/app/network_connect/MainActivity.kt
+++ AndroidBeginner/CheckingConnection/network-connect-3/src/main/java/algokelvin/app/network_connect/MainActivity.kt
@@ -1,20 +1,46 @@
 package algokelvin.app.network_connect
 
+import algokelvin.app.network_connect.databinding.ActivityMainBinding
+import android.content.Intent
 import android.net.ConnectivityManager
 import android.os.Bundle
-import android.util.Log
+import android.provider.Settings
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private var isConnected = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val isConnected = isNetworkConnected()
-        Log.i("status-connect", isConnected.toString())
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        isNetworkConnected()
+        setConnection()
     }
-    private fun isNetworkConnected(): Boolean {
+
+    override fun onResume() {
+        super.onResume()
+        isNetworkConnected()
+        setConnection()
+    }
+
+    private fun isNetworkConnected() {
         val cm = getSystemService(CONNECTIVITY_SERVICE) as ConnectivityManager
         val activeNetworkInfo = cm.activeNetworkInfo
-        return activeNetworkInfo != null && activeNetworkInfo.isConnected
+        isConnected = activeNetworkInfo != null && activeNetworkInfo.isConnected
+    }
+    private fun setConnection() {
+        if (!isConnected) {
+            binding.txtMsgConnection.text = ("Connection is Off")
+            binding.btnSetNetwork.text = ("Setting Network")
+            binding.btnSetNetwork.setOnClickListener {
+                val intentToSetNetwork = Intent(Settings.ACTION_WIRELESS_SETTINGS)
+                startActivity(intentToSetNetwork)
+            }
+        } else {
+            binding.layoutStatusConnect.visibility = View.GONE
+        }
     }
 }
