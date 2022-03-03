5ee028a29458f513b78799693d2d8e8d8bca7c81
algokelvin-373 <kelvinht373@gmail.com>
2022-03-03 20:25:34 +0700

    [Android Content App] [007] Intent Parcelable

--- AndroidContentSosMed/AndroidApp007/app/src/main/java/algokelvin/android07/intentparcelable/MainActivity.java
+++ AndroidContentSosMed/AndroidApp007/app/src/main/java/algokelvin/android07/intentparcelable/MainActivity.java
@@ -2,13 +2,39 @@ package algokelvin.android07.intentparcelable;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    private final String DATA_OBJECT = "DATA_OBJECT";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText edtName = findViewById(R.id.edt_name);
+        EditText edtAddress = findViewById(R.id.edt_address);
+        EditText edtJob = findViewById(R.id.edt_job);
+        Button btnOk = findViewById(R.id.btn_ok);
+
+        btnOk.setOnClickListener(v -> {
+            String name = edtName.getText().toString();
+            String address = edtAddress.getText().toString();
+            String job = edtJob.getText().toString();
+
+            Person person = new Person(name, address, job);
+            sendObjData(person);
+        });
+
     }
+
+    private void sendObjData(Person person) {
+        Intent toPage = new Intent(this, PageIntentActivity.class);
+        toPage.putExtra(DATA_OBJECT, person);
+        startActivity(toPage);
+    }
+
 }
