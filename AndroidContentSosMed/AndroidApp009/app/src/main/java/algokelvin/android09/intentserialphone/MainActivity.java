6c369182d54432f06731dd3333fba59afe99ecd4
algokelvin-373 <kelvinht373@gmail.com>
2022-03-18 16:44:53 +0700

    [Android Content App] [008] Intent Implisit - Serial Phone

--- AndroidContentSosMed/AndroidApp009/app/src/main/java/algokelvin/android09/intentserialphone/MainActivity.java
+++ AndroidContentSosMed/AndroidApp009/app/src/main/java/algokelvin/android09/intentserialphone/MainActivity.java
@@ -2,7 +2,11 @@ package algokelvin.android09.intentserialphone;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText edtNoPhone = findViewById(R.id.edt_no_phone);
+        Button btnToSerialPhone = findViewById(R.id.btn_go_serial_phone);
+
+        btnToSerialPhone.setOnClickListener(v -> {
+            String phone = edtNoPhone.getText().toString();
+            Intent intentSerialPhone = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:"+phone));
+            startActivity(intentSerialPhone);
+        });
+
     }
 }
