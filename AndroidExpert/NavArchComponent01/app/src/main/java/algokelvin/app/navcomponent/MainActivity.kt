43b44c4e8f605803cc87770149882dedb2ea8b27
algokelvin-373 <kelvinht373@gmail.com>
2022-03-28 20:40:44 +0700

    [Android Expert] [Navigation Arch Component] Success build and send data bundle

--- AndroidExpert/NavArchComponent01/app/src/main/java/algokelvin/app/navcomponent/MainActivity.kt
+++ AndroidExpert/NavArchComponent01/app/src/main/java/algokelvin/app/navcomponent/MainActivity.kt
@@ -1,11 +1,15 @@
 package algokelvin.app.navcomponent
 
+import algokelvin.app.navcomponent.databinding.ActivityMainBinding
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
