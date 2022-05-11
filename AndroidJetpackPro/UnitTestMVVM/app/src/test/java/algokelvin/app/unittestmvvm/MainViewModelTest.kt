7409d6b9c200716314435838d2f3aa4d285eea9d
algokelvin-373 <kelvinht373@gmail.com>
2022-05-11 14:33:07 +0700

    [Android Jetpack] [Unit Test MVVM] Check Not Equal Data

--- AndroidJetpackPro/UnitTestMVVM/app/src/test/java/algokelvin/app/unittestmvvm/MainViewModelTest.kt
+++ AndroidJetpackPro/UnitTestMVVM/app/src/test/java/algokelvin/app/unittestmvvm/MainViewModelTest.kt
@@ -4,6 +4,7 @@ import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.lifecycle.Observer
 import com.nhaarman.mockitokotlin2.verify
 import junit.framework.TestCase
+import org.junit.Assert.assertNotEquals
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -28,15 +29,29 @@ class MainViewModelTest: TestCase() {
     }
 
     @Test
-    fun createData() {
+    fun checkData() {
         val dataSample = "AlgoKelvin"
 
         val resultData = viewModel.getData(dataSample).value
         assertNotNull(resultData)
+        assertEquals(resultData, dataSample)
 
         viewModel.getData(dataSample).observeForever(observer)
         verify(observer).onChanged(dataSample)
 
+    }
+
+    @Test
+    fun checkNotEqualData() {
+        val dataSample = "AlgoKelvin"
+        val dataOther = "Kelvin"
+
+        val resultData = viewModel.getData(dataOther).value
+        assertNotNull(resultData)
+        assertNotEquals(resultData, dataSample)
+
+        viewModel.getData(dataOther).observeForever(observer)
+        verify(observer).onChanged(dataOther)
     }
 
 }
