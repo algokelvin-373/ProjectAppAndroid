51fa098b225fbe3ef0206a736b85bb3c7f310ef6
algokelvin-373 <kelvinht373@gmail.com>
2022-05-28 21:53:10 +0700

    [Android Advanced] [Count App] ViewMode, LiveData, DataBinding

--- AndroidJetpackPro/AppViewModel/CountApp/app/src/main/java/algokelvin/app/countapp/MainActivity.kt
+++ AndroidJetpackPro/AppViewModel/CountApp/app/src/main/java/algokelvin/app/countapp/MainActivity.kt
@@ -1,11 +1,27 @@
 package algokelvin.app.countapp
 
+import algokelvin.app.countapp.databinding.ActivityMainBinding
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
 
 class MainActivity : AppCompatActivity() {
+
+    private val mainViewModel by lazy {
+        ViewModelProvider(this)[MainViewModel::class.java]
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val binding: ActivityMainBinding = DataBindingUtil.setContentView(
+            this,
+            R.layout.activity_main
+        )
+
+        binding.lifecycleOwner = this
+        binding.myViewModel = mainViewModel
+
     }
 }
