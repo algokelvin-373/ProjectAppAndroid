2be5ed78b3a165dd24b948ba05008e13fa4b61cf
algokelvin-373 <kelvinht373@gmail.com>
2022-06-28 13:19:15 +0700

    [Android Beginner][Input Output Data] UI Activity and View Binding

--- AndroidBeginner/AndroidInputOutput/app/src/main/java/algokelvin/app/inputoutput/ShowActivity.java
+++ AndroidBeginner/AndroidInputOutput/app/src/main/java/algokelvin/app/inputoutput/ShowActivity.java
@@ -1,17 +1,18 @@
 package algokelvin.app.inputoutput;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 
-import algokelvin.app.inputoutput.databinding.ActivityMainBinding;
+import algokelvin.app.inputoutput.databinding.ActivityShowBinding;
 
-public class MainActivity extends AppCompatActivity {
+public class ShowActivity extends AppCompatActivity {
 
-    private ActivityMainBinding binding;
+    private ActivityShowBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        binding = ActivityShowBinding.inflate(getLayoutInflater());
         super.onCreate(savedInstanceState);
         setContentView(binding.getRoot());
     }
