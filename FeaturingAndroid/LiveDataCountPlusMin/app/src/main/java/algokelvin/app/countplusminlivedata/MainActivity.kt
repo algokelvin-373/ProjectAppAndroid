e96ec4cc6bea7be6fccd6ab85e410062fae80b8e
algokelvin-373 <kelvinht373@gmail.com>
2022-11-14 21:19:31 +0700

    [v1.2.4] Done for Project Live Data Count Plus Min Apps

--- FeaturingAndroid/LiveDataCountPlusMin/app/src/main/java/algokelvin/app/countplusminlivedata/MainActivity.kt
+++ FeaturingAndroid/LiveDataCountPlusMin/app/src/main/java/algokelvin/app/countplusminlivedata/MainActivity.kt
@@ -1,11 +1,40 @@
 package algokelvin.app.countplusminlivedata
 
+import algokelvin.app.countplusminlivedata.databinding.ActivityMainBinding
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import java.util.Random
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        main()
     }
+
+    private fun main() {
+        val controller = MainController(Random().nextInt(100))
+
+        controller.setResult().observe(this) {
+            binding.txtResult.text = getString(R.string.result, it.toString())
+        }
+
+        binding.btnPlus.setOnClickListener {
+            val data = binding.edtNumber.text.toString().toInt()
+            controller.setPlus(data).observe(this) {
+                binding.txtResult.text = getString(R.string.result, it.toString())
+            }
+        }
+
+        binding.btnMin.setOnClickListener {
+            val data = binding.edtNumber.text.toString().toInt()
+            controller.setMin(data).observe(this) {
+                binding.txtResult.text = getString(R.string.result, it.toString())
+            }
+        }
+    }
+
 }
