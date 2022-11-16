ed90a6fe10dd13c8c5370e1b713c544b24d4a2c6
algokelvin-373 <kelvinht373@gmail.com>
2022-11-17 00:09:22 +0700

    [v1.2.4] Done Implement Coroutine Basic

--- FeaturingAndroid/CoroutineApp/app/src/main/java/algokelvin/app/coroutine/CoroutineBasicActivity.kt
+++ FeaturingAndroid/CoroutineApp/app/src/main/java/algokelvin/app/coroutine/CoroutineBasicActivity.kt
@@ -1,11 +1,38 @@
 package algokelvin.app.coroutine
 
+import algokelvin.app.coroutine.databinding.ActivityCoroutineBinding
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class CoroutineBasicActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityCoroutineBinding
+
+    private var count = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_coroutine_basic)
+        binding = ActivityCoroutineBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnCount.setOnClickListener {
+            binding.txtCount.text = getString(R.string.data_count, (++count).toString())
+        }
+
+        binding.btnDownloadUserData.setOnClickListener {
+            CoroutineScope(Dispatchers.IO).launch {
+                downloadData()
+            }
+        }
+
+    }
+
+    private fun downloadData() {
+        for (i in 1 until 1000000) {
+            Log.i("ALGOKELVIN", "Downloading user $i in ${Thread.currentThread().name}")
+        }
     }
 }
