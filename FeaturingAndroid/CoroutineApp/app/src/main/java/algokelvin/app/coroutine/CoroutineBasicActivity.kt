5f589026c6f72cc5fec4c1e9205bfe11739228b5
algokelvin-373 <kelvinht373@gmail.com>
2022-11-16 23:32:16 +0700

    [v1.2.4] Create Project Coroutine Apps

---
+++ FeaturingAndroid/CoroutineApp/app/src/main/java/algokelvin/app/coroutine/CoroutineBasicActivity.kt
@@ -0,0 +1,11 @@
+package algokelvin.app.coroutine
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CoroutineBasicActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_coroutine_basic)
+    }
+}
---
+++ FeaturingAndroid/CoroutineApp/app/src/main/java/algokelvin/app/coroutine/NoCoroutineActivity.kt
@@ -0,0 +1,11 @@
+package algokelvin.app.coroutine
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NoCoroutineActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_coroutine)
+    }
+}
