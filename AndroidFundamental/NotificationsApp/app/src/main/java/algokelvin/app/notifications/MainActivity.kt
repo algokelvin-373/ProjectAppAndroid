22e46db5f1aecd36b99ae7147ee2c10cac6bb38f
algokelvin-373 <kelvinht373@gmail.com>
2022-11-20 21:59:09 +0700

    [v1.2.4] Project Notifications App - Reply Action

--- AndroidFundamental/NotificationsApp/app/src/main/java/algokelvin/app/notifications/MainActivity.kt
+++ AndroidFundamental/NotificationsApp/app/src/main/java/algokelvin/app/notifications/MainActivity.kt
@@ -10,10 +10,12 @@ import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.core.app.NotificationCompat
+import androidx.core.app.RemoteInput
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
+    private val KEY_REPLY = "key_reply"
     private val channelId = "algokelvin.app.notifications.channel01"
     private var notificationManager: NotificationManager? = null
 
@@ -54,6 +56,18 @@ class MainActivity : AppCompatActivity() {
         val btnAction: NotificationCompat.Action =
             NotificationCompat.Action.Builder(0, "Details", pendingBtnActionIntent).build()
 
+        // Reply on Notification
+        val remoteInput = RemoteInput.Builder(KEY_REPLY).run {
+            setLabel("Insert Your Name")
+            build()
+        } // for input data
+        val replyAction = NotificationCompat.Action.Builder(
+            0,
+            "REPLY",
+            pendingIntent
+        ).addRemoteInput(remoteInput)
+            .build() // for action reply
+
         val notification = NotificationCompat.Builder(this, channelId)
             .setContentTitle("Demo Title")
             .setContentText("This is a demo notification")
@@ -62,6 +76,7 @@ class MainActivity : AppCompatActivity() {
             .setPriority(NotificationCompat.PRIORITY_DEFAULT)
             .setContentIntent(pendingIntent)
             .addAction(btnAction)
+            .addAction(replyAction)
             .build()
         notificationManager?.notify(notificationId,notification)
     }
