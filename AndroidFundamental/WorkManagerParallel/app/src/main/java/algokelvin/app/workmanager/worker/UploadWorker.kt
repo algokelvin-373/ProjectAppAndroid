05482e9b57e53a6ea5210d8070749d6879c19bcb
algokelvin-373 <kelvinht373@gmail.com>
2022-11-24 23:47:48 +0700

    [v1.2.4] Create Project Work Manager Parallel

---
+++ AndroidFundamental/WorkManagerParallel/app/src/main/java/algokelvin/app/workmanager/worker/UploadWorker.kt
@@ -0,0 +1,36 @@
+package algokelvin.app.workmanager.worker
+
+import algokelvin.app.workmanager.ConstVal
+import android.content.Context
+import android.util.Log
+import androidx.work.Data
+import androidx.work.Worker
+import androidx.work.WorkerParameters
+import java.lang.Exception
+
+class UploadWorker(
+    context: Context,
+    workerParameters: WorkerParameters
+): Worker(context, workerParameters) {
+
+    override fun doWork(): Result {
+        return try {
+            val count = inputData.getInt(ConstVal.KEY_COUNT_VALUE, 0)
+            val start = System.currentTimeMillis()
+            for (i in 0..count) {
+                Log.i("AlgoKelvin", "Uploading $i")
+            }
+            val finish = System.currentTimeMillis()
+
+            val message = "Time execution: "+ (finish - start) +" ms"
+            val outputData = Data.Builder()
+                .putString(ConstVal.KEY_WORKER, message)
+                .build()
+
+            Result.success(outputData)
+        } catch (e: Exception){
+            Result.failure()
+        }
+    }
+
+}
