7fb640c4442ca6475f5de64196a2e9fe82b6f3ac
algokelvin-373 <kelvinht373@gmail.com>
2022-11-24 22:21:42 +0700

    [v1.2.4] Project Work Manager: Set Input & Output Data of Worker

--- AndroidFundamental/WorkManagerApp/app/src/main/java/algokelvin/app/workmanager/MainActivity.kt
+++ AndroidFundamental/WorkManagerApp/app/src/main/java/algokelvin/app/workmanager/MainActivity.kt
@@ -3,7 +3,9 @@ package algokelvin.app.workmanager
 import algokelvin.app.workmanager.databinding.ActivityMainBinding
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.work.Constraints
+import androidx.work.Data
 import androidx.work.NetworkType
 import androidx.work.OneTimeWorkRequest
 import androidx.work.WorkManager
@@ -23,7 +25,11 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun setOneTimeWorkRequest() {
-        val workManager = WorkManager.getInstance(applicationContext)
+        val workManager = WorkManager.getInstance(applicationContext) // Set Work Manager
+
+        val data = Data.Builder()
+            .putInt(ConstVal.KEY_COUNT_VALUE, 1500)
+            .build() // Send data integer using Work Manager
         val constraints = Constraints.Builder()
             .setRequiresCharging(true) // Work is run if charge is working
             .setRequiredNetworkType(NetworkType.CONNECTED) // Work is run if network is connected
@@ -31,11 +37,17 @@ class MainActivity : AppCompatActivity() {
 
         val uploadRequest = OneTimeWorkRequest.Builder(UploadWorker::class.java)
             .setConstraints(constraints)
+            .setInputData(data)
             .build()
 
         workManager.enqueue(uploadRequest)
         workManager.getWorkInfoByIdLiveData(uploadRequest.id).observe(this) {
             binding.txtData.text = it.state.name
+            if (it.state.isFinished) {
+                val dataOutput = it.outputData
+                val message = dataOutput.getString(ConstVal.KEY_WORKER)
+                Toast.makeText(applicationContext, message, Toast.LENGTH_LONG).show()
+            }
         }
     }
 
--- AndroidFundamental/WorkManagerApp/app/src/main/java/algokelvin/app/workmanager/UploadWorker.kt
+++ AndroidFundamental/WorkManagerApp/app/src/main/java/algokelvin/app/workmanager/UploadWorker.kt
@@ -2,6 +2,7 @@ package algokelvin.app.workmanager
 
 import android.content.Context
 import android.util.Log
+import androidx.work.Data
 import androidx.work.Worker
 import androidx.work.WorkerParameters
 import java.lang.Exception
@@ -13,10 +14,19 @@ class UploadWorker(
 
     override fun doWork(): Result {
         return try {
-            for (i in 0..500000) {
+            val count = inputData.getInt(ConstVal.KEY_COUNT_VALUE, 0)
+            val start = System.currentTimeMillis()
+            for (i in 0..count) {
                 Log.i("AlgoKelvin", "Uploading $i")
             }
-            Result.success()
+            val finish = System.currentTimeMillis()
+
+            val message = "Time execution: "+ (finish - start) +" ms"
+            val outputData = Data.Builder()
+                .putString(ConstVal.KEY_WORKER, message)
+                .build()
+
+            Result.success(outputData)
         } catch (e: Exception){
             Result.failure()
         }
