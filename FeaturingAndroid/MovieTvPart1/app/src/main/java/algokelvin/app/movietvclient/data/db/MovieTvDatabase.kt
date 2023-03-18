9a6a8e08a276680233898ed04f61a4aaf04adec3
algokelvin-373 <kelvinht373@gmail.com>
2023-03-18 16:24:19 +0700

    [1.0.0] Android MVVM Clean Architecture: Case MovieTv: Remote and Local DB (DI Not Library)

--- FeaturingAndroid/MovieTvPart1/app/src/main/java/algokelvin/app/movietvclient/data/db/MovieTvDatabase.kt
+++ FeaturingAndroid/MovieTvPart1/app/src/main/java/algokelvin/app/movietvclient/data/db/MovieTvDatabase.kt
@@ -3,7 +3,9 @@ package algokelvin.app.movietvclient.data.db
 import algokelvin.app.movietvclient.data.model.artist.Artist
 import algokelvin.app.movietvclient.data.model.movies.Movie
 import algokelvin.app.movietvclient.data.model.tvshows.TvShow
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 
 @Database(
@@ -15,4 +17,23 @@ abstract class MovieTvDatabase: RoomDatabase() {
     abstract fun movieDao(): MovieDao
     abstract fun tvShowDao(): TvShowDao
     abstract fun artisDao(): ArtistDao
+
+    companion object{
+        @Volatile
+        private var INSTANCE : MovieTvDatabase? = null
+        fun getInstance(context: Context): MovieTvDatabase {
+            synchronized(this){
+                var instance = INSTANCE
+                if(instance==null) {
+                    instance = Room.databaseBuilder(
+                        context.applicationContext,
+                        MovieTvDatabase::class.java,
+                        "movie_tv_database"
+                    ).build()
+                    INSTANCE = instance
+                }
+                return instance
+            }
+        }
+    }
 }
--- FeaturingAndroid/MovieTvPart1/app/src/main/java/algokelvin/app/movietvclient/presentation/movie/MovieActivity.kt
+++ FeaturingAndroid/MovieTvPart1/app/src/main/java/algokelvin/app/movietvclient/presentation/movie/MovieActivity.kt
@@ -1,6 +1,13 @@
 package algokelvin.app.movietvclient.presentation.movie
 
+import algokelvin.app.movietvclient.BuildConfig
 import algokelvin.app.movietvclient.R
+import algokelvin.app.movietvclient.data.api.MovieTvServices
+import algokelvin.app.movietvclient.data.api.RetrofitInstance
+import algokelvin.app.movietvclient.data.repository.movies.datasource.MovieRemoteDataSource
+import algokelvin.app.movietvclient.data.repository.movies.datasourceImpl.MovieLocalDataSourceImpl
+import algokelvin.app.movietvclient.data.repository.movies.datasourceImpl.MovieRemoteDataSourceImpl
+import algokelvin.app.movietvclient.data.repository.movies.datasourceImpl.MovieRepositoryImpl
 import algokelvin.app.movietvclient.databinding.ActivityMovieBinding
 import algokelvin.app.movietvclient.domain.usecase.movie.GetMoviesUseCase
 import algokelvin.app.movietvclient.domain.usecase.movie.UpdateMoviesUseCase
@@ -11,6 +18,19 @@ import androidx.lifecycle.ViewModelProvider
 
 class MovieActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMovieBinding
+
+    private val movieService by lazy {
+        RetrofitInstance.getRetrofitInstance().create(MovieTvServices::class.java)
+    }
+
+
+    private val movieRemoteDataSource = MovieRemoteDataSourceImpl(movieService, BuildConfig.URL)
+    private val movieLocalDataSource = MovieLocalDataSourceImpl()
+
+    private val movieRepository = MovieRepositoryImpl(
+        movieRemoteDataSource,
+        movieLocalDataSource,
+    )
 
     private val factory by lazy {
         MovieViewModelFactory(GetMoviesUseCase(), UpdateMoviesUseCase())
