8dbcb56909db0f779f508913fa48c5bac63e8198
algokelvin-373 <kelvinht373@gmail.com>
2023-04-02 00:46:50 +0700

    [1.0.0] Android Jetpack Compose: The Basically of Jetpack Compose Android

--- FeaturingAndroid/JetpackComposeTwo/app/src/main/java/algokelvin/app/jetpackcompose/MainActivity.kt
+++ FeaturingAndroid/JetpackComposeTwo/app/src/main/java/algokelvin/app/jetpackcompose/MainActivity.kt
@@ -16,15 +16,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            JetpackComposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            DefaultPreview()
         }
     }
 }
@@ -38,6 +30,11 @@ fun Greeting(name: String) {
 @Composable
 fun DefaultPreview() {
     JetpackComposeTheme {
-        Greeting("Android")
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colors.background
+        ) {
+            Greeting("Android Jetpack")
+        }
     }
 }
