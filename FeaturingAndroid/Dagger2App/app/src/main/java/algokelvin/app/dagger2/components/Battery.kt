f104fb0b97e58227f94dc22b084c0957ff7ba21c
algokelvin-373 <kelvinht373@gmail.com>
2023-04-01 18:18:47 +0700

    [1.0.0] Android Dagger 2: Injection with Interface

--- FeaturingAndroid/Dagger2App/app/src/main/java/algokelvin/app/dagger2/components/Battery.kt
+++ FeaturingAndroid/Dagger2App/app/src/main/java/algokelvin/app/dagger2/components/Battery.kt
@@ -1,14 +1,5 @@
 package algokelvin.app.dagger2.components
 
-import android.util.Log
-import javax.inject.Inject
-
-class Battery @Inject constructor() {
-    init {
-        Log.i("MYTAG","Battery Constructed")
-    }
-
-    fun getPower(){
-        Log.i("MYTAG","Battery power is available")
-    }
+interface Battery {
+    fun getPower()
 }
--- FeaturingAndroid/Dagger2App/app/src/main/java/algokelvin/app/dagger2/components/SmartPhoneComponent.kt
+++ FeaturingAndroid/Dagger2App/app/src/main/java/algokelvin/app/dagger2/components/SmartPhoneComponent.kt
@@ -1,8 +1,9 @@
 package algokelvin.app.dagger2.components
 
+import algokelvin.app.dagger2.battery.NCBatteryModule
 import dagger.Component
 
-@Component(modules = [MemoryCardModule::class])
+@Component(modules = [MemoryCardModule::class, NCBatteryModule::class])
 interface SmartPhoneComponent {
     fun getSmartPhone(): SmartPhone
 }
