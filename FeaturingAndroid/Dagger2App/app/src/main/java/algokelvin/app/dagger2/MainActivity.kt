0b79964904a12e3ef932bdbde974052b41660401
algokelvin-373 <kelvinht373@gmail.com>
2023-04-01 22:23:41 +0700

    [1.0.0] Android Dagger 2: Injection for Activity

--- FeaturingAndroid/Dagger2App/app/src/main/java/algokelvin/app/dagger2/MainActivity.kt
+++ FeaturingAndroid/Dagger2App/app/src/main/java/algokelvin/app/dagger2/MainActivity.kt
@@ -4,18 +4,25 @@ import algokelvin.app.dagger2.components.DaggerSmartPhoneComponent
 import algokelvin.app.dagger2.components.SmartPhone
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import javax.inject.Inject
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var smartPhone: SmartPhone
+    @Inject
+    lateinit var smartPhone: SmartPhone
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Using Dagger
+        // Using Dagger - Inject with this activity
         DaggerSmartPhoneComponent.create()
+            .inject(this)
+        smartPhone.makeACallWithRecording()
+
+        // Using Dagger - Inject function program
+        /*DaggerSmartPhoneComponent.create()
             .getSmartPhone()
-            .makeACallWithRecording()
+            .makeACallWithRecording()*/
 
         // This is code DI No Use Dagger 2
         /*
--- FeaturingAndroid/Dagger2App/app/src/main/java/algokelvin/app/dagger2/components/SmartPhoneComponent.kt
+++ FeaturingAndroid/Dagger2App/app/src/main/java/algokelvin/app/dagger2/components/SmartPhoneComponent.kt
@@ -1,9 +1,14 @@
 package algokelvin.app.dagger2.components
 
+import algokelvin.app.dagger2.MainActivity
 import algokelvin.app.dagger2.battery.NCBatteryModule
 import dagger.Component
 
 @Component(modules = [MemoryCardModule::class, NCBatteryModule::class])
 interface SmartPhoneComponent {
-    fun getSmartPhone(): SmartPhone
+    // for injection on main activity
+    fun inject(mainActivity: MainActivity)
+
+    // for injection method getSmartPhone
+    /*fun getSmartPhone(): SmartPhone*/
 }
