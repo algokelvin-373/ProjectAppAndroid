09c22ba299cff5a08749d8905db69d58bf397a0d
algokelvin-373 <kelvinht373@gmail.com>
2023-04-02 19:57:55 +0700

    [1.0.0] Android Jetpack Compose: Button View Jetpack Compose

--- FeaturingAndroid/JetpackComposeTwo/app/src/main/java/algokelvin/app/jetpackcompose/ColumnLayoutActivity.kt
+++ FeaturingAndroid/JetpackComposeTwo/app/src/main/java/algokelvin/app/jetpackcompose/ColumnLayoutActivity.kt
@@ -20,7 +20,7 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
-class MainActivity : ComponentActivity() {
+class ColumnLayoutActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
