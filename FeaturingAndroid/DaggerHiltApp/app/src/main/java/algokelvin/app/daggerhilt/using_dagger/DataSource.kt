1c50c2470750a9085ffec8889cdb4bff096535a6
algokelvin-373 <kelvinht373@gmail.com>
2023-04-15 22:09:52 +0700

    [1.0.0] Amdroid Dagger Hilt: The Introduction

---
+++ FeaturingAndroid/DaggerHiltApp/app/src/main/java/algokelvin/app/daggerhilt/using_dagger/DataSource.kt
@@ -0,0 +1,9 @@
+package algokelvin.app.daggerhilt.using_dagger
+
+import android.util.Log
+
+class DataSource {
+    fun getRemoteData() {
+        Log.i("AlgoKelvin", "Data downloading...")
+    }
+}
