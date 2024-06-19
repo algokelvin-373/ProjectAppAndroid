d8873447c528e788e121c278d1994a9857eb46dd
algokelvin-373 <kelvinht373@gmail.com>
2024-06-19 07:47:21 +0700

    Show Dialog - Clean Code and Layout for Dialog

--- ShowDialog/app/src/main/java/com/algokelvin/showdialog/utils/DialogBottomController.java
+++ ShowDialog/app/src/main/java/com/algokelvin/showdialog/utils/DialogBottomController.java
@@ -6,8 +6,8 @@ import com.google.android.material.bottomsheet.BottomSheetDialog;
 
 public class DialogBottomController {
     private BottomSheetDialog bottomSheetDialog;
-    private Context context;
-    private int layoutDialogBottom;
+    private final Context context;
+    private final int layoutDialogBottom;
 
     public DialogBottomController(Context context, int layoutDialogBottom) {
         this.context = context;
