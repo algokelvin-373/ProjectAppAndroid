e3e619ffeb6ebea2bb2bbaa62da99a04f332e0dc
algokelvin-373 <kelvinht373@gmail.com>
2024-09-10 23:58:15 +0700

    [Navigation Architecture Component] Init project for Navigation Architecture Component

---
+++ BootCamp_Udemy/NavigationArchitecture/app/src/main/java/com/algokelvin/navarch/MainActivity.java
@@ -0,0 +1,21 @@
+package com.algokelvin.navarch;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.algokelvin.navarch.ui.main.MainFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow();
+        }
+    }
+}
