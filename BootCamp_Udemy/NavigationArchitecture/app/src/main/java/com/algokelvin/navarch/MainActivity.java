29ced13991cf366db66a60bd18829da411dd8a38
algokelvin-373 <kelvinht373@gmail.com>
2024-09-12 16:41:59 +0700

    [Navigation Architecture Component] Navigation Destination

--- BootCamp_Udemy/NavigationArchitecture/app/src/main/java/com/algokelvin/navarch/MainActivity.java
+++ BootCamp_Udemy/NavigationArchitecture/app/src/main/java/com/algokelvin/navarch/MainActivity.java
@@ -1,22 +1,25 @@
 package com.algokelvin.navarch;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 
 import com.algokelvin.navarch.ui.main.MainFragment;
 import com.algokelvin.navarch.ui.main.SecondFragment;
 
-public class MainActivity extends AppCompatActivity implements SecondFragment.OnFragmentInteractionListener {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        if (savedInstanceState == null) {
+        /*if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
                     .replace(R.id.container, MainFragment.newInstance())
                     .commitNow();
-        }
+        }*/
     }
 }
