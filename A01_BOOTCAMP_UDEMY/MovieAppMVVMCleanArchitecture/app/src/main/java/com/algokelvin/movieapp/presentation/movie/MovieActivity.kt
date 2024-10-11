fcb6749426f38b24a732c5744ca7c20d3cdf7a66
algokelvin-373 <kelvinht373@gmail.com>
2024-10-11 22:33:36 +0700

    [Movie App MVVM Clean Architecture] Presentation Layer - Set Adapter and Get All Data Movie TvShow and Artist

--- A01_BOOTCAMP_UDEMY/MovieAppMVVMCleanArchitecture/app/src/main/java/com/algokelvin/movieapp/presentation/movie/MovieActivity.kt
+++ A01_BOOTCAMP_UDEMY/MovieAppMVVMCleanArchitecture/app/src/main/java/com/algokelvin/movieapp/presentation/movie/MovieActivity.kt
@@ -2,10 +2,13 @@ package com.algokelvin.movieapp.presentation.movie
 
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.algokelvin.movieapp.R
 import com.algokelvin.movieapp.databinding.ActivityMovieBinding
 import com.algokelvin.movieapp.presentation.di.Injector
@@ -17,6 +20,7 @@ class MovieActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMovieBinding
     private lateinit var movieViewModel: MovieViewModel
+    private lateinit var adapter: MovieAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,9 +30,27 @@ class MovieActivity : AppCompatActivity() {
             .inject(this)
         movieViewModel = ViewModelProvider(this, factory)[MovieViewModel::class]
 
-        val responseListMovie = movieViewModel.getMovies()
-        responseListMovie.observe(this, Observer {
-            Log.i("ALGOKELVIN_INFO", it.toString())
+        initRecyclerView()
+    }
+    private fun initRecyclerView(){
+        binding.movieRecyclerView.layoutManager = LinearLayoutManager(this)
+        adapter = MovieAdapter()
+        binding.movieRecyclerView.adapter = adapter
+        displayPopularMovies()
+    }
+
+    private fun displayPopularMovies(){
+        binding.movieProgressBar.visibility = View.VISIBLE
+        val responseLiveData = movieViewModel.getMovies()
+        responseLiveData.observe(this, Observer {
+            if(it!=null){
+                adapter.setList(it)
+                adapter.notifyDataSetChanged()
+                binding.movieProgressBar.visibility = View.GONE
+            }else{
+                binding.movieProgressBar.visibility = View.GONE
+                Toast.makeText(applicationContext,"No data available", Toast.LENGTH_LONG).show()
+            }
         })
     }
 }
--- A01_BOOTCAMP_UDEMY/MovieAppMVVMCleanArchitecture/app/src/main/java/com/algokelvin/movieapp/presentation/tv/TvShowActivity.kt
+++ A01_BOOTCAMP_UDEMY/MovieAppMVVMCleanArchitecture/app/src/main/java/com/algokelvin/movieapp/presentation/tv/TvShowActivity.kt
@@ -1,16 +1,57 @@
 package com.algokelvin.movieapp.presentation.tv
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.algokelvin.movieapp.R
 import com.algokelvin.movieapp.databinding.ActivityTvShowBinding
+import com.algokelvin.movieapp.presentation.di.Injector
+import com.algokelvin.movieapp.presentation.movie.MovieAdapter
+import javax.inject.Inject
 
 class TvShowActivity : AppCompatActivity() {
+    @Inject
+    lateinit var factory: TvShowViewModelFactory
+
     private lateinit var binding: ActivityTvShowBinding
+    private lateinit var tvShowViewModel: TvShowViewModel
+    private lateinit var adapter: TvShowAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_tv_show)
+
+        (application as Injector).createTvShowSubComponent()
+            .inject(this)
+        tvShowViewModel = ViewModelProvider(this, factory)[TvShowViewModel::class]
+
+        initRecyclerView()
+    }
+
+    private fun initRecyclerView(){
+        binding.tvShowRecyclerView.layoutManager = LinearLayoutManager(this)
+        adapter = TvShowAdapter()
+        binding.tvShowRecyclerView.adapter = adapter
+        displayPopularMovies()
+    }
+
+    private fun displayPopularMovies(){
+        binding.tvShowProgressBar.visibility = View.VISIBLE
+        val responseLiveData = tvShowViewModel.getTvShows()
+        responseLiveData.observe(this, Observer {
+            if(it!=null){
+                adapter.setList(it)
+                adapter.notifyDataSetChanged()
+                binding.tvShowProgressBar.visibility = View.GONE
+            }else{
+                binding.tvShowProgressBar.visibility = View.GONE
+                Toast.makeText(applicationContext,"No data available", Toast.LENGTH_LONG).show()
+            }
+        })
     }
 }
