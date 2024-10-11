a7ed0784992e8a5195892a0195a5f411ab8baf0a
algokelvin-373 <kelvinht373@gmail.com>
2024-10-11 14:04:10 +0700

    [Movie App MVVM Clean Architecture] Data Layer -  Create Data Movie TvShow and Artist

---
+++ A01_BOOTCAMP_UDEMY/MovieAppMVVMCleanArchitecture/app/src/main/java/com/algokelvin/movieapp/data/MovieList.kt
@@ -0,0 +1,15 @@
+package com.algokelvin.movieapp.data
+
+
+import com.google.gson.annotations.SerializedName
+
+data class MovieList(
+    @SerializedName("page")
+    val page: Int,
+    @SerializedName("results")
+    val results: List<Movie>,
+    @SerializedName("total_pages")
+    val totalPages: Int,
+    @SerializedName("total_results")
+    val totalResults: Int
+)
---
+++ A01_BOOTCAMP_UDEMY/MovieAppMVVMCleanArchitecture/app/src/main/java/com/algokelvin/movieapp/data/TvShowList.kt
@@ -0,0 +1,15 @@
+package com.algokelvin.movieapp.data
+
+
+import com.google.gson.annotations.SerializedName
+
+data class TvShowList(
+    @SerializedName("page")
+    val page: Int,
+    @SerializedName("results")
+    val results: List<TvShow>,
+    @SerializedName("total_pages")
+    val totalPages: Int,
+    @SerializedName("total_results")
+    val totalResults: Int
+)
