3b5ddb3391f7ec1617e2a3da86160dd618abf428
algokelvin-373 <kelvinht373@gmail.com>
2024-10-13 00:23:05 +0700

    Delete Project is not Used

--- A03_APP_OTHER/MovieTvPart1/app/src/main/java/algokelvin/app/movietvclient/data/repository/artists/datasource/ArtistCacheDataSource.kt
+++
@@ -1,8 +0,0 @@
-package algokelvin.app.movietvclient.data.repository.artists.datasource
-
-import algokelvin.app.movietvclient.data.model.artist.Artist
-
-interface ArtistCacheDataSource {
-    suspend fun getArtistsFromCache(): List<Artist>
-    suspend fun saveArtistsFromCache(artists: List<Artist>)
-}
--- A03_APP_OTHER/MovieTvPart2/app/src/main/java/algokelvin/app/movietvclient/domain/repository/MovieRepository.kt
+++
@@ -1,8 +0,0 @@
-package algokelvin.app.movietvclient.domain.repository
-
-import algokelvin.app.movietvclient.data.model.movies.Movie
-
-interface MovieRepository {
-    suspend fun getMovies(): List<Movie>?
-    suspend fun updateMovies(): List<Movie>?
-}
