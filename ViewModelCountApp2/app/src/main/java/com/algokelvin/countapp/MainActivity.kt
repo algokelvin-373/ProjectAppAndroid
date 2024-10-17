74f83acac99f95380e65c6b8d3b7acff3880d3e6
algokelvin-373 <kelvinht373@gmail.com>
2024-10-17 11:44:43 +0700

    [Count App] Done to Develop Count App

--- ViewModelCountApp2/app/src/main/java/com/algokelvin/countapp/MainActivity.kt
+++ ViewModelCountApp2/app/src/main/java/com/algokelvin/countapp/MainActivity.kt
@@ -1,20 +1,27 @@
 package com.algokelvin.countapp
 
-import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
+import com.algokelvin.countapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val mainViewModel by lazy {
+        ViewModelProvider(this)[MainViewModel::class.java]
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        val binding: ActivityMainBinding = DataBindingUtil.setContentView(
+            this,
+            R.layout.activity_main
+        )
+
+        binding.lifecycleOwner = this
+        binding.myViewModel = mainViewModel
+
     }
 }
