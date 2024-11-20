21d1a4950da0c0a4a0adda8e76e666ab3a42af74
algokelvin-373 <kelvinht373@gmail.com>
2024-11-20 20:56:55 +0700

    Add App Other from Repo App-PrintManager

---
+++ A03_APP_OTHER/PrinterManager/src/com/example/printersample/SamplePrintActivity.kt
@@ -0,0 +1,11 @@
+package com.example.printersample
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class SamplePrintActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sample_print)
+    }
+}
