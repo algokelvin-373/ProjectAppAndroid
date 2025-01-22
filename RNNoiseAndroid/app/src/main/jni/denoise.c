8a63d4d7fdf09055177b7c027fc223ecd21a38fd
algokelvin-373 <kelvinht373@gmail.com>
2025-01-22 10:11:43 +0700

    [RNN Android] Import Native NDK for RNNoise cpp

---
+++ RNNoiseAndroid/app/src/main/jni/denoise.c
@@ -0,0 +1,30 @@
+//
+// Created by fryant on 2018/12/23.
+//
+
+#include <jni.h>
+#include <string.h>
+#include <stdio.h>
+#include "main.h"
+
+#define FRAME_SIZE 480
+
+#ifndef _Included_com_example_jni_jnitest_MainActivity
+#define _Included_com_example_jni_jnitest_MainActivity
+#ifdef __cplusplus
+extern "C"{
+#endif
+
+JNIEXPORT jint JNICALL
+Java_com_fryant_denoise_MainActivity_rnnoise(JNIEnv *env, jobject instance,jstring infile,jstring outfile) {
+
+    const char *infi=(*env)->GetStringUTFChars(env,infile, JNI_FALSE);
+    const char *outf=(*env)->GetStringUTFChars(env,outfile, JNI_FALSE);
+    dn(infi,outf);
+    return 0;
+}
+
+#ifdef __cplusplus
+}
+#endif
+#endif
