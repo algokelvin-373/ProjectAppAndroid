7f83ef78dc702e17c5df69d530b4d39766cde6f8
algokelvin-373 <kelvinht373@gmail.com>
2025-01-27 23:10:42 +0700

    [Math 2D 3D] Develop UI Part 1

--- Math2D3D/app/src/main/java/com/algokelvin/math2d3d/MainActivity.kt
+++ Math2D3D/app/src/main/java/com/algokelvin/math2d3d/MainActivity.kt
@@ -2,10 +2,14 @@ package com.algokelvin.math2d3d
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.algokelvin.math2d3d.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
