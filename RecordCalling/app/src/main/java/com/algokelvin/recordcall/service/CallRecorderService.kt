3a1e49580997bc8b518c08e36055465dda5422a1
algokelvin-373 <kelvinht373@gmail.com>
2025-01-30 17:51:56 +0700

    [Research - RecordCalling] Dev Part 3 - Show notification when record call is active

--- RecordCalling/app/src/main/java/com/algokelvin/recordcall/service/CallRecorderService.kt
+++ RecordCalling/app/src/main/java/com/algokelvin/recordcall/service/CallRecorderService.kt
@@ -1,18 +1,27 @@
 package com.algokelvin.recordcall.service
 
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.app.Service
 import android.content.Intent
 import android.media.MediaRecorder
+import android.os.Build
 import android.os.Environment
 import android.os.IBinder
 import android.telephony.PhoneStateListener
 import android.telephony.TelephonyManager
 import android.util.Log
+import androidx.core.app.NotificationCompat
 
 class CallRecorderService : Service() {
     private val TAG = "RecordCallingLogger"
     private var mediaRecorder: MediaRecorder? = null
     private var filename: String = ""
+
+    // Channel ID untuk notifikasi
+    private val CHANNEL_ID = "CallRecorderChannel"
+    private val NOTIFICATION_ID = 123
 
     private val phoneStateListener = object : PhoneStateListener() {
         override fun onCallStateChanged(state: Int, phoneNumber: String?) {
@@ -26,13 +35,41 @@ class CallRecorderService : Service() {
     override fun onBind(intent: Intent?): IBinder? = null
 
     override fun onCreate() {
+        super.onCreate()
+        createNotificationChannel()
+
         val telephonyManager = getSystemService(TELEPHONY_SERVICE) as TelephonyManager
         telephonyManager.listen(phoneStateListener, PhoneStateListener.LISTEN_CALL_STATE)
+    }
+
+    private fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val serviceChannel = NotificationChannel(
+                CHANNEL_ID,
+                "Call Recorder Channel",
+                NotificationManager.IMPORTANCE_DEFAULT
+            )
+
+            val manager = getSystemService(NotificationManager::class.java)
+            manager.createNotificationChannel(serviceChannel)
+        }
+    }
+
+    private fun buildNotification(): Notification {
+        return NotificationCompat.Builder(this, CHANNEL_ID)
+            .setContentTitle("Sedang Merekam Panggilan")
+            .setContentText("Tap untuk membuka aplikasi")
+            .setSmallIcon(android.R.drawable.ic_menu_call)
+            .setOngoing(true)
+            .build()
     }
 
     private fun startRecording() {
         Log.i(TAG, "startRecording")
         try {
+            // Mulai foreground service
+            startForeground(NOTIFICATION_ID, buildNotification())
+
             mediaRecorder = MediaRecorder().apply {
                 setAudioSource(MediaRecorder.AudioSource.VOICE_CALL)
                 setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP)
