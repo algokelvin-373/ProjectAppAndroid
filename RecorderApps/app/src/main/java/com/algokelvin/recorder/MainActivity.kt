7744cd7c25b54660ace2f8bd43b8e435006c4644
algokelvin-373 <kelvinht373@gmail.com>
2025-02-04 11:46:40 +0700

    [Record App] Repair develop 2

--- RecorderApps/app/src/main/java/com/algokelvin/recorder/MainActivity.kt
+++ RecorderApps/app/src/main/java/com/algokelvin/recorder/MainActivity.kt
@@ -18,7 +18,6 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.algokelvin.recorder.databinding.ActivityMainBinding
-import com.tsm.recorder.ConstantVal
 import java.io.File
 import java.io.IOException
 
