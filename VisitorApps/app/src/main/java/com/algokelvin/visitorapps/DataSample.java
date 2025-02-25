58f29938ae64df26c621898d161b0ecfbbf8612c
algokelvin-373 <kelvinht373@gmail.com>
2025-02-25 13:35:31 +0700

    2 New Project Has Been Developed - TimerApps and VisitorApps

---
+++ VisitorApps/app/src/main/java/com/algokelvin/visitorapps/DataSample.java
@@ -0,0 +1,94 @@
+package com.algokelvin.visitorapps;
+
+import java.util.ArrayList;
+
+public class DataSample {
+    public static ArrayList<Visitor> setData() {
+        ArrayList<Visitor> data = new ArrayList<>();
+        data.add(new Visitor(1, "10 Desember 2022", "Kelvin Herwanda Tandrio",
+                "Jl. Merak No.51 Jaten, Jawa Tengah"));
+        data.add(new Visitor(2, "10 Desember 2022", "Fransisco",
+                "Jl. Meteng Kalibata, Jakarta Barat"));
+        data.add(new Visitor(3, "10 Desember 2022", "Yulia Grace",
+                "Tangerang, Banten"));
+        data.add(new Visitor(4, "10 Desember 2022", "Veren Valencia",
+                "Solo Creator, Tangerang Selatan"));
+        data.add(new Visitor(5, "10 Desember 2022", "Thea B",
+                "Jl. Kemayoran, Jakarta Utara, DKI Jakarta"));
+        data.add(new Visitor(6, "08 Desember 2022", "Fransisco",
+                "Jl. Meteng Kalibata, Jakarta Barat"));
+        data.add(new Visitor(7, "08 Desember 2022", "Yulia Grace",
+                "Tangerang, Banten"));
+        data.add(new Visitor(8, "08 Desember 2022", "Veren Valencia",
+                "Solo Creator, Tangerang Selatan"));
+        data.add(new Visitor(9, "07 Desember 2022", "Yulia Grace",
+                "Tangerang, Banten"));
+        data.add(new Visitor(10, "07 Desember 2022", "Veren Valencia",
+                "Solo Creator, Tangerang Selatan"));
+        data.add(new Visitor(11, "07 Desember 2022", "Thea B",
+                "Jl. Kemayoran, Jakarta Utara, DKI Jakarta"));
+        data.add(new Visitor(12, "04 Desember 2022", "Kelvin Herwanda Tandrio",
+                "Jl. Merak No.51 Jaten, Jawa Tengah"));
+        data.add(new Visitor(13, "04 Desember 2022", "Fransisco",
+                "Jl. Meteng Kalibata, Jakarta Barat"));
+        data.add(new Visitor(14, "03 Desember 2022", "Kelvin Herwanda Tandrio",
+                "Jl. Merak No.51 Jaten, Jawa Tengah"));
+        data.add(new Visitor(15, "03 Desember 2022", "Fransisco",
+                "Jl. Meteng Kalibata, Jakarta Barat"));
+        data.add(new Visitor(16, "03 Desember 2022", "Yulia Grace",
+                "Tangerang, Banten"));
+        data.add(new Visitor(17, "03 Desember 2022", "Veren Valencia",
+                "Solo Creator, Tangerang Selatan"));
+        data.add(new Visitor(18, "02 Desember 2022", "Kelvin Herwanda Tandrio",
+                "Jl. Merak No.51 Jaten, Jawa Tengah"));
+        data.add(new Visitor(19, "01 Desember 2022", "Veren Valencia",
+                "Solo Creator, Tangerang Selatan"));
+        data.add(new Visitor(20, "01 Desember 2022", "Thea B",
+                "Jl. Kemayoran, Jakarta Utara, DKI Jakarta"));
+        return data;
+    }
+    public static ArrayList<Visitor> setData2() {
+        ArrayList<Visitor> data = new ArrayList<>();
+        data.add(new Visitor(1, "28 November 2022", "Kelvin Herwanda Tandrio",
+                "Jl. Merak No.51 Jaten, Jawa Tengah"));
+        data.add(new Visitor(2, "28 November 2022", "Fransisco",
+                "Jl. Meteng Kalibata, Jakarta Barat"));
+        data.add(new Visitor(3, "25 November 2022", "Yulia Grace",
+                "Tangerang, Banten"));
+        data.add(new Visitor(4, "25 November 2022", "Veren Valencia",
+                "Solo Creator, Tangerang Selatan"));
+        data.add(new Visitor(5, "25 November 2022", "Thea B",
+                "Jl. Kemayoran, Jakarta Utara, DKI Jakarta"));
+        data.add(new Visitor(6, "21 November 2022", "Fransisco",
+                "Jl. Meteng Kalibata, Jakarta Barat"));
+        data.add(new Visitor(7, "21 November 2022", "Yulia Grace",
+                "Tangerang, Banten"));
+        data.add(new Visitor(8, "21 November 2022", "Veren Valencia",
+                "Solo Creator, Tangerang Selatan"));
+        data.add(new Visitor(9, "21 November 2022", "Yulia Grace",
+                "Tangerang, Banten"));
+        data.add(new Visitor(10, "15 November 2022", "Veren Valencia",
+                "Solo Creator, Tangerang Selatan"));
+        data.add(new Visitor(11, "14 November 2022", "Thea B",
+                "Jl. Kemayoran, Jakarta Utara, DKI Jakarta"));
+        data.add(new Visitor(12, "14 November 2022", "Kelvin Herwanda Tandrio",
+                "Jl. Merak No.51 Jaten, Jawa Tengah"));
+        data.add(new Visitor(13, "14 November 2022", "Fransisco",
+                "Jl. Meteng Kalibata, Jakarta Barat"));
+        data.add(new Visitor(14, "11 November 2022", "Kelvin Herwanda Tandrio",
+                "Jl. Merak No.51 Jaten, Jawa Tengah"));
+        data.add(new Visitor(15, "11 November 2022", "Fransisco",
+                "Jl. Meteng Kalibata, Jakarta Barat"));
+        data.add(new Visitor(16, "08 November 2022", "Yulia Grace",
+                "Tangerang, Banten"));
+        data.add(new Visitor(17, "08 November 2022", "Veren Valencia",
+                "Solo Creator, Tangerang Selatan"));
+        data.add(new Visitor(18, "08 November 2022", "Kelvin Herwanda Tandrio",
+                "Jl. Merak No.51 Jaten, Jawa Tengah"));
+        data.add(new Visitor(19, "08 November 2022", "Veren Valencia",
+                "Solo Creator, Tangerang Selatan"));
+        data.add(new Visitor(20, "08 November 2022", "Thea B",
+                "Jl. Kemayoran, Jakarta Utara, DKI Jakarta"));
+        return data;
+    }
+}
