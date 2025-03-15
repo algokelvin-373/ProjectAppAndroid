454bce693803d14c520a6b76765cfa612e7dc6eb
algokelvin-373 <kelvinht373@gmail.com>
2025-03-15 21:18:10 +0700

    [UI Compose] Compose Text and Image

--- UiCompose/app/src/main/java/com/algokelvin/ui/compose/MainActivity.kt
+++ UiCompose/app/src/main/java/com/algokelvin/ui/compose/MainActivity.kt
@@ -4,13 +4,25 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.algokelvin.ui.compose.ui.theme.UiComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -24,10 +36,32 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun TitleText(name: String) {
     Text(
-        text = "SHello $name!",
-        modifier = modifier
+        text = "Hello $name!",
+        color = Color.Black,
+        fontSize = 20.sp,
+        fontWeight = FontWeight.Bold,
+    )
+}
+
+@Composable
+fun SubTitleText(name: String) {
+    Text(
+        text = name,
+        color = Color.Black,
+        fontSize = 16.sp,
+    )
+}
+
+@Composable
+fun ImageIcon(id: Int) {
+    Image(
+        painter = painterResource(id = id),
+        contentDescription = "Sample Image",
+        modifier = Modifier
+            .size(300.dp)
+            .padding(bottom = 16.dp)
     )
 }
 
@@ -35,11 +69,18 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun UiComposeLayout() {
     UiComposeTheme {
-        Scaffold(Modifier.fillMaxSize()) { innerPadding ->
-            Greeting(
-                name = "Android",
-                modifier = Modifier.padding(innerPadding)
-            )
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            TitleText("Jetpack Compose")
+            Box(modifier = Modifier.height(2.dp))
+            SubTitleText("My Logo is Jetpack Compose")
+            Box(modifier = Modifier.height(2.dp))
+            ImageIcon(R.drawable.ic_compose)
         }
     }
 }
