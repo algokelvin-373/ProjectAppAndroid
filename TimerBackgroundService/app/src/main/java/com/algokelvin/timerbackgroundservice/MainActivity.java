de00606a1e86f6334990e92d8e887a8b1c069050
algokelvin-373 <kelvinht373@gmail.com>
2025-07-09 18:00:25 +0700

    [TimerBackgroundService] Develop 3 - Service Created

--- TimerBackgroundService/app/src/main/java/com/algokelvin/timerbackgroundservice/MainActivity.java
+++ TimerBackgroundService/app/src/main/java/com/algokelvin/timerbackgroundservice/MainActivity.java
@@ -1,7 +1,9 @@
 package com.algokelvin.timerbackgroundservice;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
@@ -9,8 +11,11 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class MainActivity extends Activity {
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+public class MainActivity extends Activity {
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1;
     private static final String TAG = "TimerServices";
 
     private TextView tvTimer;
@@ -27,14 +32,26 @@ public class MainActivity extends Activity {
         btnGo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent serviceIntent = new Intent(MainActivity.this, TimerService.class);
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                    startForegroundService(serviceIntent);
-                }
+                startTimerService();
             }
         });
 
         Log.d(TAG, "MainActivity onCreate");
+    }
+
+    private void startTimerService() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q &&
+                ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                        != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    LOCATION_PERMISSION_REQUEST_CODE);
+        } else {
+            Intent serviceIntent = new Intent(MainActivity.this, TimerService.class);
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                startForegroundService(serviceIntent);
+            }
+        }
     }
 
     @Override
