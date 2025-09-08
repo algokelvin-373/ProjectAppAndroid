07b52a24a5f1664f2794c9c8a98af539a6fd153a
algokelvin-373 <kelvinht373@gmail.com>
2025-09-08 14:02:47 +0700

    [Retrofit Coroutine] Update ENV + Delete old env

---
+++ RetrofitCoroutine/app/src/main/java/com/algokelvin/retrofitcoroutine/model/AlbumsItem.kt
@@ -0,0 +1,7 @@
+package com.algokelvin.retrofitcoroutine.model
+
+data class AlbumsItem(
+    val id: Int,
+    val title: String,
+    val userId: Int
+)
--- RetrofitCoroutineApp/app/src/main/java/algokelvin/app/retrofitcoroutine/model/AlbumsItem.kt
+++
@@ -1,7 +0,0 @@
-package algokelvin.app.retrofitcoroutine.model
-
-data class AlbumsItem(
-    val id: Int,
-    val title: String,
-    val userId: Int
-)
