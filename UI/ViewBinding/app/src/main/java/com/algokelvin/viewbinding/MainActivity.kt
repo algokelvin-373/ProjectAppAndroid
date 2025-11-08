e8893b0f6ee41cb3101a9af55c202f368ce48838
algokelvin-373 <kelvinht373@gmail.com>
2025-11-09 00:49:05 +0700

    [View Binding] Dev

--- UI/ViewBinding/app/src/main/java/com/algokelvin/viewbinding/MainActivity.kt
+++ UI/ViewBinding/app/src/main/java/com/algokelvin/viewbinding/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.algokelvin.viewbinding
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.algokelvin.viewbinding.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnAction.setOnClickListener({
+            Toast.makeText(this, "你点击这个按钮", Toast.LENGTH_LONG)
+                .show()
+        })
     }
 }
