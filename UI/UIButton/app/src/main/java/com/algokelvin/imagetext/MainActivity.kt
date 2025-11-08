7a1ad13e12f31b44ae3a8ddf4ef773bde0f7c337
algokelvin-373 <kelvinht373@gmail.com>
2025-11-08 23:34:08 +0700

    [UIButton] Update

--- UI/UIButton/app/src/main/java/com/algokelvin/imagetext/MainActivity.kt
+++ UI/UIButton/app/src/main/java/com/algokelvin/imagetext/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.algokelvin.imagetext
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.algokelvin.imagetext.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnAction = findViewById<Button>(R.id.btn_action)
+        btnAction.setOnClickListener({
+            Toast.makeText(this, "你点击这个按钮", Toast.LENGTH_LONG).show()
+        })
     }
 }
