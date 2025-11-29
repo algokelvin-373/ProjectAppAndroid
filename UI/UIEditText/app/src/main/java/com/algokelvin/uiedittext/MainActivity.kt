403b7d23bc9cf5861cc35aaaac7f93674382657d
algokelvin-373 <kelvinht373@gmail.com>
2025-11-29 21:34:16 +0700

    [UI EditText] Dev - Implement View Binding

--- UI/UIEditText/app/src/main/java/com/algokelvin/uiedittext/MainActivity.kt
+++ UI/UIEditText/app/src/main/java/com/algokelvin/uiedittext/MainActivity.kt
@@ -3,30 +3,31 @@ package com.algokelvin.uiedittext
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.algokelvin.uiedittext.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val editText = findViewById<EditText>(R.id.editTextInput)
-        val buttonShow = findViewById<Button>(R.id.buttonShow)
-        val output = findViewById<TextView>(R.id.textViewOutput)
+        val editText = binding.editTextInput
+        val buttonShow = binding.buttonShow
+        val output = binding.textViewOutput
 
         // Update output TextView as user types
         editText.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                output.text = getString(R.string.output_label, s ?: "")
+                output.text = getString(R.string.output_label, s?.toString() ?: "")
             }
 
             override fun afterTextChanged(s: Editable?) {}
@@ -38,7 +39,7 @@ class MainActivity : AppCompatActivity() {
             Toast.makeText(this, text.ifEmpty { "(empty)" }, Toast.LENGTH_SHORT).show()
         }
 
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
