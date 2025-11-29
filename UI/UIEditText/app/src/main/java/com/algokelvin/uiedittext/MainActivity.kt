9cab7b5fb4396f3cdd26118000a0c1cf9a8ce37c
algokelvin-373 <kelvinht373@gmail.com>
2025-11-29 21:24:18 +0700

    [UI EditText] Dev - Logic EditText

--- UI/UIEditText/app/src/main/java/com/algokelvin/uiedittext/MainActivity.kt
+++ UI/UIEditText/app/src/main/java/com/algokelvin/uiedittext/MainActivity.kt
@@ -1,6 +1,12 @@
 package com.algokelvin.uiedittext
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,6 +17,27 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        val editText = findViewById<EditText>(R.id.editTextInput)
+        val buttonShow = findViewById<Button>(R.id.buttonShow)
+        val output = findViewById<TextView>(R.id.textViewOutput)
+
+        // Update output TextView as user types
+        editText.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                output.text = getString(R.string.output_label, s ?: "")
+            }
+
+            override fun afterTextChanged(s: Editable?) {}
+        })
+
+        // On button click show a Toast with current text
+        buttonShow.setOnClickListener {
+            val text = editText.text.toString()
+            Toast.makeText(this, text.ifEmpty { "(empty)" }, Toast.LENGTH_SHORT).show()
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
